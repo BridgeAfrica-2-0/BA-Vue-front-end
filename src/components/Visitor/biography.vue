9b9e12a7d5ac5206d6600176fb4d52d67d26adce
unknown <njolaifomo@gmail.com>
2021-01-22 08:21:43 +0100

    COMMENT TO DESCRIBE THE INTENTION OF THE COMMIT

---
+++ src/components/Visitor/biography.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <b-alert v-if="edited" show> {{ successmsg }} </b-alert>
+    <b-button
+      v-if="editing == false"
+      class="edit-btn"
+      @click="edit(true)"
+      variant="outline-primary"
+      >Edit Profile</b-button
+    >
+    <p v-if="editing == false">
+      {{ biography }}
+    </p>
+    <div v-if="editing">
+      <b-form @submit="save">
+        <div style="width: 150px">
+          <b-form-select
+            required
+            class="mb-2"
+            size="sm"
+            v-model="selected"
+            :options="options"
+          ></b-form-select>
+        </div>
+        <b-form-textarea
+          required
+          id="textarea"
+          v-model="biography"
+          placeholder="Enter something..."
+          rows="3"
+          max-rows="6"
+        ></b-form-textarea>
+        <div class="pt-2 action-btn">
+          <b-button variant="success" type="submit">Save</b-button>
+          <b-button class="ml-2" variant="primary" @click="edit(false)"
+            >Cancel</b-button
+          >
+        </div>
+      </b-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "",
+      editing: false,
+      selected: null,
+      successmsg: "",
+      edited: false,
+      biography:
+        " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, aliquid cupiditate fugit similique laboriosam, expedita, voluptatibus dolorem atlibero ipsam illo molestiae. Voluptates quisquam vitae, aperiam voluptatem ad enim ducimus praesentium fuga quas unde ea quasi obcaecati eumlaboriosam eos nesciunt exercitationem voluptatibus cupiditate sunt totam?Dolor voluptatem repudiandae quos.",
+      options: [
+        { value: null, text: "Select" },
+        { value: "private", text: "Private" },
+        { value: "public", text: "Public" },
+      ],
+    };
+  },
+  methods: {
+    edit(state) {
+      this.editing = state;
+    },
+    save() {
+      this.edited = true;
+      this.successmsg = "Profile was sccesfully Edited";
+      this.editing = false;
+      setInterval(() => {
+        this.edited = false;
+      }, 1000);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.edit-btn {
+  margin-left: 660px;
+  margin-bottom: 20px;
+}
+.action-btn {
+  margin-left: 80%;
+}
+
+@media only screen and (max-width: 600px) {
+  .edit-btn {
+    margin-left: 129px;
+  }
+  .action-btn {
+    display: flex;
+    flex-direction: row;
+    margin-left: 38%;
+  }
+}
+</style>
---
+++ src/components/Visitor/workplace.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <b>WorkPlace</b>
+    <hr />
+    <b-link style="text-decoration: none" class="mt-4" v-b-modal.modal-9>
+      <b-icon icon="plus" variant="primary"></b-icon>
+      Add Workplace</b-link
+    >
+    <b-list-group-item
+      class="d-flex align-items-center mb-4 "
+      style="border: none"
+    >
+      <b-avatar class="mr-3 avatar"></b-avatar>
+      <div class="datails">
+        <div class="row">
+          <div class="col">
+            <span class="mr-auto"> <b>Company</b> </span>
+            <p>Position</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+              quibusdam harum maxime!
+            </p>
+          </div>
+          <div class="col-1">
+            <b-dropdown id="dropdown-dropup" dropdown variant="primary-outline">
+              <b-dropdown-item href="#">Edit</b-dropdown-item>
+              <b-dropdown-item href="#">Delete</b-dropdown-item>
+            </b-dropdown>
+          </div>
+        </div>
+      </div>
+    </b-list-group-item>
+
+    <b-modal ref="add-contact" id="modal-9" title="Add Workplace ">
+      <div style="width: 100px">
+        <b-form-select
+          class="mb-2"
+          size="sm"
+          v-model="selected"
+          :options="options"
+        ></b-form-select>
+      </div>
+      <b-form-input
+        class="mt-2"
+        v-model="hometown"
+        placeholder="Company"
+      ></b-form-input>
+      <b-form-input
+        class="mt-2"
+        v-model="hometown"
+        placeholder="Position"
+      ></b-form-input>
+      <b-form-input
+        class="mt-2"
+        v-model="hometown"
+        placeholder="City"
+      ></b-form-input>
+      <b-form-input
+        class="mt-2"
+        v-model="hometown"
+        placeholder="Responsibilities"
+      ></b-form-input>
+
+      <b-form-checkbox
+        id="checkbox-1"
+        v-model="status"
+        name="checkbox-1"
+        value="duration"
+        unchecked-value="not_accepted"
+      >
+        Currently Working
+      </b-form-checkbox>
+      <label>Duration</label>
+
+      <b-form-datepicker
+        id="example-datepicker"
+        v-model="duration"
+        class="mb-2"
+      ></b-form-datepicker>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      status: "not_accepted",
+      selected: null,
+      options: [
+        { value: null, text: "Select" },
+        { value: "private", text: "Private" },
+        { value: "public", text: "Public" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.avatar {
+  position: relative;
+  top: -50px;
+}
+@media only screen and (max-width: 600px) {
+  .avatar {
+    position: relative;
+    top: -100px;
+  }
+}
+</style>

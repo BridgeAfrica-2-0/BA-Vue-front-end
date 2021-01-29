b589d7ece0325a4f5aa286680f1b48100a867799
Tabe Denisdeon <tabedenideon4@gmail.com>
2021-01-29 18:09:50 +0100

    refactored primes code

---
+++ src/components/intro.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+         <b-card title="" header-tag="header" footer-tag="footer">
+          <template #header>
+            <h6 class="mb-0">Intro</h6>
+          </template>
+          <b-card-text>
+            <p>
+              <b-icon icon="briefcase-fill" variant="primary"></b-icon> Work at:
+              Current or Last Organization
+            </p>
+            <p>
+              <b-icon icon="book-fill" variant="primary"></b-icon> Studied at:
+              Last Education
+            </p>
+            <p>
+              <b-icon icon="house-fill" variant="primary"></b-icon> Home Town
+              Name:Dummy
+            </p>
+            <p>
+              <b-icon icon="geo-alt-fill" variant="primary"></b-icon> Current
+              City Name:Dummy
+            </p>
+            <p>
+              <b-icon icon="people-fill" variant="primary"></b-icon> Followed
+              by: 525
+            </p>
+          </b-card-text>
+        </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/media.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+       <b-card class="mt-4" header-tag="header" footer-tag="footer">
+          <template #header>
+            <h6 class="mb-0 d-inline-block">Media</h6>
+            <span class="float-right"
+              ><b-icon icon="eye-fill" variant="primary"></b-icon> See All</span
+            >
+          </template>
+          <b-card-text>
+            <b-row>
+              <b-col cols="6" v-for="i in 4" :key="i" class="p-1">
+                <img
+                  class="mb-2 img-fluid"
+                  src="@/assets/img/m2.jpg"
+                  alt="Fluid image"
+                />
+              </b-col>
+            </b-row>
+          </b-card-text>
+        </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

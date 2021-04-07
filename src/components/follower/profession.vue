a58f52911c8aa0ab1d823dc12730124e9c0c12ad
blezour <blezourblec@gmail.com>
2021-04-07 01:28:05 +0100

    updates

--- src/components/follower/profession.vue
+++ src/components/follower/profession.vue
@@ -2,11 +2,7 @@
   <div>
     <b class="profession">Profession</b>
     <hr />
-<<<<<<< HEAD
     <b-list-group-item class="d-flex align-items-center mb-4 item">
-=======
-    <b-list-group-item class="d-flex align-items-center mb-4 border-none">
->>>>>>> f155289a77178ac4585f3b7a9f2568ae722d414d
       <div class="datails">
         <div class="row">
           <div class="col">

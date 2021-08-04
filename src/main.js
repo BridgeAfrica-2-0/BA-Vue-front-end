7298024310a4d6fc5dce6d6646684668df069786
Sir-Deon <sird655@gmail.com>
2021-08-04 10:59:56 +0100

    finished configuring firebase

--- src/main.js
+++ src/main.js
@@ -4,72 +4,74 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-
+import axios from "axios";
+import VueAxios from "vue-axios";
 import VueGallerySlideshow from "vue-gallery-slideshow";
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-import { fab } from '@fortawesome/free-brands-svg-icons'
-
-
-
-
-import vSelect from 'vue-select';
-
-
-import Vuex from 'vuex';
-
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import vSelect from "vue-select";
+import Vuex from "vuex";
+import IconifyIcon from "@iconify/vue";
+import homeIconData from "@iconify-icons/mdi-light/home";
+import ReadMore from "vue-read-more";
+import VueSocialauth from "vue-social-auth";
+import firebase from "firebase";
 Vue.use(Vuex);
 Vue.use(VueAxios, axios);
-
-
-import IconifyIcon from '@iconify/vue';
-
-import homeIconData from '@iconify-icons/mdi-light/home';
-
-IconifyIcon.addIcon('home', homeIconData);
-
-
-import ReadMore from 'vue-read-more';
-
 Vue.use(ReadMore);
+IconifyIcon.addIcon("home", homeIconData);
+Vue.prototype.$axios = axios;
 
-import VueSocialauth from 'vue-social-auth'
+const firebaseConfig = {
+  apiKey: "AIzaSyDu9rL6_YDSeTyU89tF8JcI9kWNR6617Fg",
+  authDomain: "bridge-africa-api.firebaseapp.com",
+  projectId: "bridge-africa-api",
+  storageBucket: "bridge-africa-api.appspot.com",
+  messagingSenderId: "50055115922",
+  appId: "1:50055115922:web:81e9b59a354a0c6e9ee24b",
+  measurementId: "G-9K2WHP9Y13",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging
+  .requestPermission()
+  .then(() => {
+    return messaging.getToken();
+  })
+  .then((token) => {
+    console.log(token);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 Vue.use(VueSocialauth, {
   providers: {
     facebook: {
-      clientId: '513850343026598',
-      client_secret: 'f126ad31262665d481b6080f7f5c645f',
-      redirectUri: 'http//:localhost:8080/login'
-    }
-  }
+      clientId: "513850343026598",
+      client_secret: "f126ad31262665d481b6080f7f5c645f",
+      redirectUri: "http//:localhost:8080/login",
+    },
+  },
 });
 
-
-
-
-
-
-import VueMaterial from 'vue-material'
+import VueMaterial from "vue-material";
 //import 'vue-material/dist/vue-material.min.css'
 //import 'vue-material/dist/theme/default.css'
 
-Vue.use(VueMaterial)
-
-
+Vue.use(VueMaterial);
 
 import Lightbox from "@morioh/v-lightbox";
-import * as VueGoogleMaps from 'vue2-google-maps';
+import * as VueGoogleMaps from "vue2-google-maps";
 
-import VueSplide from '@splidejs/vue-splide';
+import VueSplide from "@splidejs/vue-splide";
 Vue.use(VueSplide);
-import '@splidejs/splide/dist/css/themes/splide-default.min.css';
-
-
+import "@splidejs/splide/dist/css/themes/splide-default.min.css";
 
 // global register
 Vue.use(VueGallerySlideshow);
@@ -77,51 +79,38 @@ Vue.use(Lightbox);
 
 // Configuring font awesome
 
-library.add(fab)
+library.add(fab);
 library.add(fas);
 
-Vue.component('fas-icon', FontAwesomeIcon);
-Vue.component('fab-icon', FontAwesomeIcon);
+Vue.component("fas-icon", FontAwesomeIcon);
+Vue.component("fab-icon", FontAwesomeIcon);
 
-
-
-import vueCountryRegionSelect from 'vue-country-region-select';
+import vueCountryRegionSelect from "vue-country-region-select";
 Vue.use(vueCountryRegionSelect);
 
+import VueFormWizard from "vue-form-wizard";
 
-
-import VueFormWizard from 'vue-form-wizard'
-
-Vue.use(VueFormWizard)
-
-
+Vue.use(VueFormWizard);
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
-import 'vue-select/dist/vue-select.css';
+import "vue-select/dist/vue-select.css";
 
 import "@/assets/css/bootstrap.css";
 
 import "@/assets/css/bootstrap.css";
-
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
-
-
-
-
-
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyAGZU6cqra18t1fhN1AbzRsEc_pgt7n2C8',
-    libraries: 'places',
-  }
+    key: "AIzaSyAGZU6cqra18t1fhN1AbzRsEc_pgt7n2C8",
+    libraries: "places",
+  },
 });
 
-Vue.component('v-select', vSelect);
-
+Vue.component("v-select", vSelect);
 
 Vue.config.productionTip = false;
 
@@ -129,24 +118,22 @@ new Vue({
   router,
   store,
 
-
   created() {
-    const userInfo = localStorage.getItem('user')
+    const userInfo = localStorage.getItem("user");
     if (userInfo) {
-      const userData = JSON.parse(userInfo)
-      this.$store.commit('auth/setUserData', userData)
+      const userData = JSON.parse(userInfo);
+      this.$store.commit("auth/setUserData", userData);
     }
     axios.interceptors.response.use(
-      response => response,
-      error => {
+      (response) => response,
+      (error) => {
         if (error.response.status === 401) {
-          this.$store.dispatch('logout')
+          this.$store.dispatch("logout");
         }
-        return Promise.reject(error)
+        return Promise.reject(error);
       }
-    )
+    );
   },
 
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
-

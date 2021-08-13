6cd9b58ecb96992d36b676c0d0df7a170d92b673
monthebrice2000 <monthedjeumoubrice2000@gmail.com>
2021-08-13 12:25:45 +0100

    Transition to begin the implementation of the Profile_about Task after User_Profile_Post

--- src/components/owner/biography.vue
+++ src/components/owner/biography.vue
@@ -4,7 +4,7 @@
     <b-button
       v-if="!editing"
       class="edit-btn float-right"
-      @click="edit"
+      @click="edit(1)"
       variant="outline-primary"
       size="sm"
       >Edit Profile</b-button
@@ -12,9 +12,10 @@
     <br />
     <br />
     <br />
-<!--    <p v-if="true">-->
-<!--      {{ biography }}-->
-<!--    </p>-->
+    <!--    <p v-if="true">-->
+    <!--      {{ biography }}-->
+    <!--    </p>-->
+
     <div v-if="editing">
       <b-form @submit="save">
         <div style="width: 150px">
@@ -22,21 +23,21 @@
             required
             class="mb-2"
             size="sm"
-            v-model="selected"
+            v-model="biography.info_access"
             :options="options"
           ></b-form-select>
         </div>
         <b-form-textarea
           required
           id="textarea"
-          v-model="biography"
+          v-model="biography.description"
           placeholder="Enter something..."
           rows="3"
           max-rows="6"
         ></b-form-textarea>
         <div class="pt-2 action-btn">
           <b-button variant="success" type="submit">Save</b-button>
-          <b-button class="ml-2" variant="primary" @click="edit(false)"
+          <b-button class="ml-2" variant="primary" @click="edit(0)"
             >Cancel</b-button
           >
         </div>
@@ -47,33 +48,75 @@
 
 <script>
 export default {
+  created() {
+    console.log("Load User Biography +++++++");
+    this.$store.dispatch('loadUserBiography', null).then( response => {
+      console.log( response )
+      this.biography = JSON.parse( JSON.stringify( this.$store.getters.getProfileAboutBiography ));
+      console.log(this.biography);
+      console.log( 'Load User Biography +++++++')
+    })
+
+  },
   data() {
     return {
-      text: "",
       editing: false,
-      selected: null,
       successmsg: "",
       edited: false,
-      biography:
-        " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, aliquid cupiditate fugit similique laboriosam, expedita, voluptatibus dolorem atlibero ipsam illo molestiae. Voluptates quisquam vitae, aperiam voluptatem ad enim ducimus praesentium fuga quas unde ea quasi obcaecati eumlaboriosam eos nesciunt exercitationem voluptatibus cupiditate sunt totam?Dolor voluptatem repudiandae quos.",
+      access: null,
       options: [
         { value: null, text: "Select" },
         { value: "private", text: "Private" },
         { value: "public", text: "Public" }
-      ]
+      ],
+      biography: {
+        info_access: null,
+        description: null
+      }
     };
   },
   methods: {
-    edit() {
+    edit(value) {
+      if( value === 0){
+        //console.log(JSON.parse(JSON.stringify( this.$store.getters.getProfileAboutBiography )) )
+        console.log('Update Biography User Cancel');
+        this.biography = JSON.parse(JSON.stringify( this.$store.getters.getProfileAboutBiography ))
+        //const { info_access, description } = this.$store.getters.getProfileAboutBiography;
+        console.log( this.biography )
+        //this.biography.info_access = info_access;
+        //this.biography.description = description;
+        this.edited = true;
+        this.successmsg = "Profile was succesfully Cancelled";
+        setInterval(() => {
+          this.edited = false;
+        }, 2000);
+      }
       this.editing = !this.editing;
+
     },
     save() {
       this.edited = true;
-      this.successmsg = "Profile was sccesfully Edited";
+      this.successmsg = "Profile was succesfully Edited";
       this.editing = false;
       setInterval(() => {
         this.edited = false;
-      }, 1000);
+      }, 2000);
+      console.log("profile about save message+++++++++");
+      console.log(this.biography);
+      this.$store
+        .dispatch("updateUserBiography", {
+          info_access: this.biography.info_access,
+          description: this.biography.description
+        })
+        .then(response => {
+          console.log("edit user biography after response +++++");
+          console.log(response);
+          console.log("edit user biography endddd ++++");
+        })
+        .catch(error => {
+          console.log("erreur lie au navigateur ou au serveur");
+          console.log(error);
+        });
     }
   }
 };

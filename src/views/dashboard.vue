1978a912765cb633f0133b49221469bdf69535f3
Titcheu Wilfried <pierretypw@gmail.com>
2021-08-19 11:21:32 +0100

    test

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -2,161 +2,111 @@
   <div style="overflow-x:hidden">
     <navbar></navbar>
 
-   
-
     <div class="text-justify  p-card ">
-
       <CarousselDashboard class="mm-top" /> <br />
-
-    
-
 
       <br />
 
-    
-     
-
-      
-     
-
-
-
-
-
-        <div  v-if="selectedb == 'owner'" >
-  
-<b-card class=" border shadow hselect" >   
-
-
-   
-   <b-row>    <b-col  md="6" sm="12" class="">   <h6 class="font-weight-bolder text-design">
-                Use Bridge Africa as Yourself or as one of your businesses
-              </h6>   </b-col>    <b-col sm="12" md="6" class="mb-3">    <b-form-select
-                v-model="selectedb"
-                :options="boptions"
-              ></b-form-select>
-            </b-col>    </b-row>
-      
-
-        </b-card>
-     <br />
-  </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-     
-
       <div v-if="selectedb == 'owner'">
-        <b-row class=" p-0">
-          <b-col md="6" sm="12"  class="mt-2 "> <Profile /> </b-col> <b-col md="6" sm="12"   class="mt-2  "> <EmptyBusiness /> </b-col>
-        </b-row>
-        <br />
-      </div>
-
-      
-
-      <div v-if="selectedb != 'owner'">
-        <b-row>
-          <b-col md="6" sm="12"    class="mt-2">    
-
-
-             <div   >
-     
-
-
-
-<b-card class=" border shadow" style="height:350px">   
-
-
-
-      
-       
-          
+        <b-card class=" border shadow hselect">
+          <b-row>
+            <b-col md="6" sm="12" class="">
               <h6 class="font-weight-bolder text-design">
                 Use Bridge Africa as Yourself or as one of your businesses
               </h6>
+            </b-col>
+            <b-col sm="12" md="6" class="mb-3">
               <b-form-select
                 v-model="selectedb"
                 :options="boptions"
               ></b-form-select>
-          
-        
-       
-
-
-       
-
-       
-
+            </b-col>
+          </b-row>
         </b-card>
-    
-  </div>    </b-col> <b-col md="6" sm="12"    class="mt-2"> <Map /> </b-col>
-        </b-row>
-
-      <br />
+        <br />
       </div>
 
-      <div  v-if="selectedb != 'owner'">
-        <b-row>   
-        
-          <b-col md="6" sm="12"    class="mt-2"> <BusinessDashboard /> </b-col> <b-col md="6" sm="12"    class="mt-2"> <Insights /> </b-col>
-      
+      <div v-if="selectedb == 'owner'">
+        <b-row class=" p-0">
+          <b-col md="6" sm="12" class="mt-2 "> <Profile /> </b-col>
+          <b-col md="6" sm="12" class="mt-2  "> <EmptyBusiness /> </b-col>
+        </b-row>
+        <br />
+      </div>
+
+      <div v-if="selectedb != 'owner'">
+        <b-row>
+          <b-col md="6" sm="12" class="mt-2">
+            <div>
+              <b-card class=" border shadow" style="height:350px">
+                <h6 class="font-weight-bolder text-design">
+                  Use Bridge Africa as Yourself or as one of your businesses
+                </h6>
+                <b-form-select
+                  v-model="selectedb"
+                  :options="boptions"
+                ></b-form-select>
+              </b-card>
+            </div>
+          </b-col>
+          <b-col md="6" sm="12" class="mt-2"> <Map /> </b-col>
+        </b-row>
+
+        <br />
+      </div>
+
+      <div v-if="selectedb != 'owner'">
+        <b-row>
+          <b-col md="6" sm="12" class="mt-2"> <BusinessDashboard /> </b-col>
+          <b-col md="6" sm="12" class="mt-2"> <Insights /> </b-col>
         </b-row>
       </div>
       <br />
 
-      <b-row>  <comuniti-dashboard class="m-component m-3"></comuniti-dashboard> <br />   </b-row>
-
-      
+      <b-row>
+        <comuniti-dashboard class="m-component m-3"></comuniti-dashboard> <br />
+      </b-row>
 
       <div>
         <b-row>
-          <b-col sm="12" lg="8"  class="mt-3" > <CommunityActivity /> </b-col>
-          <b-col sm="12" lg="4"    class="mt-3"> <Tutorial /> </b-col>
+          <b-col sm="12" lg="8" class="mt-3"> <CommunityActivity /> </b-col>
+          <b-col sm="12" lg="4" class="mt-3"> <Tutorial /> </b-col>
         </b-row>
       </div>
       <br />
 
       <div>
         <b-row>
-          <b-col sm="12" lg="6"   class="mt-3" >   
-            
-            
-              <b-card class="border shadow card  card-hight"> 
+          <b-col sm="12" lg="6" class="mt-3">
+            <b-card class="border shadow card  card-hight">
+              <h6 class=" titlle">
+                <fas-icon
+                  class="icons"
+                  :icon="['fas', 'hands-helping']"
+                  size="lg"
+                />
+                <b> HOT BUSINESSES </b>
+              </h6>
 
+              <div class="  p-tab">
+                <Business />
+              </div>
+            </b-card>
+          </b-col>
+          <b-col sm="12" lg="6" class="mt-3">
+            <b-card class="border shadow  card card-hight">
+              <h6 class=" titlle">
+                <fas-icon
+                  class=" icons"
+                  :icon="['fas', 'project-diagram']"
+                  size="lg"
+                />
+                <b> POPULAR NETWORKS </b>
+              </h6>
 
-         <h6 class=" titlle">   <fas-icon class="icons" :icon="['fas', 'hands-helping']" size="lg" />    <b>    HOT BUSINESSES  </b>  </h6>  
-
-
-
-
-           <div class="  p-tab">
-             
-                <Business /> 
-           
-                
-             
-             
-           </div>
-              </b-card >
-                 </b-col> <b-col sm="12" lg="6"  class="mt-3" >          <b-card class="border shadow  card card-hight">    
-    
-
-
-
-    <h6 class=" titlle">  <fas-icon class=" icons" :icon="['fas', 'project-diagram']"  size="lg" />  <b>   POPULAR NETWORKS </b>    </h6>  
-
- <div class="">      <Popularnetwork />      </div>  </b-card>                   </b-col>
+              <div class=""><Popularnetwork /></div>
+            </b-card>
+          </b-col>
         </b-row>
       </div>
     </div>
@@ -187,9 +137,6 @@ import Popularnetwork from "@/components/dasboard/popularnetwork";
 //import Hotbusiness from "@/components/dasboard/hotbusiness";
 //import People from '../components/businessOwner/tabs/owner/networks/people.vue';
 
-
-
-
 export default {
   name: "dashboard",
   data() {
@@ -201,8 +148,8 @@ export default {
       location: "any",
       category: "any",
       post: "any",
-       selectedb: "owner",
-      
+      selectedb: "owner",
+
       educatio: "any",
 
       professio: "any",
@@ -211,15 +158,12 @@ export default {
 
       map: false,
 
-
-       boptions: [
+      boptions: [
         { value: "owner", text: "Owner's Name" },
         { value: "a", text: "Business Name 1" },
         { value: "b", text: "Business Name 2" },
         { value: "c", text: "Business Name 3" }
       ],
-
-
 
       optionss: [
         { id: "all", label: "All" },
@@ -301,113 +245,75 @@ export default {
     Popularnetwork,
     //Hotbusiness,
     //SelectBusiness,
-    
+
     Map,
-    
+
     EmptyBusiness,
     //SelectBH,
     Profile,
-    
-    
+
     CarousselDashboard,
     Navbar
-  
   },
   methods: {}
 };
-</script>    
+</script>
 
-<style  >
-   .icon-color{ color:#e75c18;}
-
-   .icons{
-   color:#e75c18;
-   width:24px;
+<style>
+.icon-color {
+  color: #e75c18;
 }
 
+.icons {
+  color: #e75c18;
+  width: 24px;
+}
 </style>
 
 <style scoped>
-
-
-.card-body{
-  padding:8px;
+.card-body {
+  padding: 8px;
 }
 
-
-.card{
-  padding:0px;
+.card {
+  padding: 0px;
   padding-top: 15px;
-
 }
-
-
 
 @media only screen and (min-width: 768px) {
-
-  
-  .titlle{
+  .titlle {
     font-size: 20px;
   }
+}
 
-  
-
-   
- }
-
-
-
-
- 
- 
 @media only screen and (max-width: 768px) {
-
-  
-  .titlle{
+  .titlle {
     font-size: 16px;
   }
+}
 
+.card-hight {
+  height: 550px;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
 
-  
-
-
-   
- }
-
-
- .card-hight{
-   height:550px;
-   overflow-y: auto;
-   overflow-x: hidden;
- }
-
-
-
-.logo-img{
-
+.logo-img {
   width: 60px;
 }
 
 @media only screen and (max-width: 768px) {
-  
+  h4 {
+    font-size: 15px;
+  }
 
-
-
-h4{
-  font-size: 15px;
+  .logo-img {
+    width: 30px;
+  }
+  .see-all-link {
+    font-size: 10px;
+  }
 }
-
-.logo-img{
-  width: 30px;
-}
-.see-all-link{
-  font-size: 10px;
-}
-
-}
-
-
-
 
 .p-card {
   margin: 2px;
@@ -430,21 +336,12 @@ h4{
   height: 50px;
 }
 
-
-
-
 select option {
   margin: 40px;
   background: white;
   color: black;
   text-shadow: 0 1px 0 rgba(0, 0, 0, 0.4);
 }
-
-
-
-
-
-
 
 .wi-25 {
   width: 25%;
@@ -461,7 +358,7 @@ select option {
 }
 
 .custom-select {
-display:inherit;
+  display: inherit;
 }
 
 .color-sit {
@@ -481,8 +378,6 @@ select {
 
   margin-top: 15px;
 }
-
-
 
 .div-h {
   height: 350px;

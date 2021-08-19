f7703d7aee46464921a82bf1e087f8bb3cd8cd18
Titcheu Wilfried <pierretypw@gmail.com>
2021-08-19 11:40:25 +0100

    Dashboard(Hotbusiness)

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,49 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import auth from "./auth";
 
 import axios from "axios";
 
 Vue.use(Vuex);
+axios.defaults.baseURL = "https://e4f5730e80f9.ngrok.io/api/v1";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/api/v1/";
+const getDefaultState = () => {
+  return {
+    token: "5|ZrS4NiZ9yXrSZ5b80aqtNsnijikEfUUXBMF51R7N",
+    bdetails: []
+  };
+};
+
+// initial state
+const state = getDefaultState();
+
+const actions = {
+  getbdetails({ commit }) {
+    return axios
+      .get("", {
+        headers: { Authorization: `Bearer ${state.token}` }
+      })
+      .then(function({ data }) {
+        commit("set_details", data.data);
+        console.log(data);
+      });
+  }
+};
+
+const mutations = {
+  set_details(state, bdetails) {
+    state.bdetails = bdetails;
+  }
+};
+
+const getters = {
+  getdetails(state) {
+    return state.bdetails;
+  }
+};
 
 export default new Vuex.Store({
-  modules: {
-    auth
-  }
+  state,
+  getters,
+  actions,
+  mutations
 });
--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -25,7 +25,7 @@
         </b-card>
         <br />
       </div>
-
+      .
       <div v-if="selectedb == 'owner'">
         <b-row class=" p-0">
           <b-col md="6" sm="12" class="mt-2 "> <Profile /> </b-col>
@@ -115,18 +115,21 @@
 
 <script>
 import Navbar from "@/components/navbar";
+
 import CarousselDashboard from "@/components/dasboard/carousselDashboard";
 
 import BusinessDashboard from "@/components/dasboard/businessDashboard";
+
 import ComunitiDashboard from "@/components/dasboard/comunitiDashboard";
+
 import Insights from "@/components/dasboard/insights";
+
 import CommunityActivity from "@/components/dasboard/communityActivity";
 
 import Tutorial from "@/components/dasboard/tutorial";
 
 import Profile from "@/components/dasboard/profile";
 
-//import People from "@/components/dasboard/communityMember";
 import Business from "@/components/dasboard/communitybusiness";
 
 import Map from "@/components/dasboard/map";
@@ -134,27 +137,16 @@ import Map from "@/components/dasboard/map";
 import EmptyBusiness from "@/components/dasboard/emptybusiness";
 
 import Popularnetwork from "@/components/dasboard/popularnetwork";
-//import Hotbusiness from "@/components/dasboard/hotbusiness";
-//import People from '../components/businessOwner/tabs/owner/networks/people.vue';
 
 export default {
   name: "dashboard",
   data() {
     return {
       slide: 0,
+
       sliding: null,
 
-      selected: "all",
-      location: "any",
-      category: "any",
-      post: "any",
       selectedb: "owner",
-
-      educatio: "any",
-
-      professio: "any",
-
-      workplac: "any",
 
       map: false,
 
@@ -163,74 +155,6 @@ export default {
         { value: "a", text: "Business Name 1" },
         { value: "b", text: "Business Name 2" },
         { value: "c", text: "Business Name 3" }
-      ],
-
-      optionss: [
-        { id: "all", label: "All" },
-        { id: "business", label: "Business" },
-        { id: "people", label: "People" },
-        { id: "network", label: "Network" },
-        { id: "marketplace", label: "Markeplace", notEnabled: true },
-        { id: "posts", label: "Posts" }
-      ],
-
-      options: [
-        { item: "all", name: "All" },
-        { item: "business", name: "Business" },
-        { item: "people", name: "People" },
-        { item: "network", name: "Network" },
-        { item: "marketplace", name: "Markeplace", notEnabled: false },
-        { item: "posts", name: "Posts" }
-      ],
-      filters: [
-        { item: "any", name: "Location : Any" },
-        { item: "yaounde", name: "Yaounde, Cameroon" },
-        { item: "dhaka ", name: "Dhaka, Bangladesh" },
-        { item: "new york", name: "New York, United States" },
-        { item: "douala", name: "Douala, Cameroon" },
-        { item: "karachi", name: "Karachi, Pakistan" }
-      ],
-      categories: [
-        { item: "any", name: "Category : Any" },
-        { item: "restaurant", name: "Restaurant" },
-        { item: "home service", name: "Home Services" },
-        { item: "auto service", name: "Auto Services" },
-        { item: "argiculture", name: "Agriculture" },
-        { item: "technology", name: "Technology" }
-      ],
-      posts: [
-        { item: "any", name: "Post: Any" },
-        { item: "people", name: "People I am following" },
-        { item: "business", name: "Businesses i am following" },
-        { item: "networks", name: "Networks I am following" }
-      ],
-
-      education: [
-        { item: "any", name: "Education: Any" },
-        { item: "ub", name: "University Of Buea" },
-        { item: "uba", name: "University Of Yaounde" },
-        { item: "uy", name: "University Of Yaounde 1" }
-      ],
-
-      profession: [
-        { item: "any", name: "Profession: Any" },
-        { item: "engineer", name: "Engineen" },
-        { item: "teacher", name: "Teacher" },
-        { item: "farmer", name: "Farmer" }
-      ],
-
-      workplace: [
-        { item: "any", name: "Post: Any" },
-        { item: "yaounde", name: "Yaounde" },
-        { item: "douala", name: "Douala" },
-        { item: "Buea", name: "Buea" }
-      ],
-
-      sponsoredBusinesses: [
-        { title: "Business 1" },
-        { title: "Business 2" },
-        { title: "Business 3" },
-        { title: "Business 4" }
       ]
     };
   },

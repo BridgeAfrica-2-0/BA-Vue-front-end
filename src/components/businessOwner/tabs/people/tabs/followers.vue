7f6f37e6bb5a41799cd3e72f0a008d1ab27246ac
Sir-Deon <sird655@gmail.com>
2021-08-25 06:14:27 -0400

    setup chat

--- src/components/businessOwner/tabs/people/tabs/followers.vue
+++ src/components/businessOwner/tabs/people/tabs/followers.vue
@@ -1,86 +1,41 @@
 <template>
   <div>
-
-            <b-row>
-
-           
-      <b-col cols="12" md="6" >
-
-
+    <b-row>
+      <b-col cols="12" md="6">
         <b-input-group class="mb-2 px-md-3 float-right">
-         
           <b-form-input
             aria-label="Text input with checkbox"
             placeholder="Search Something"
           ></b-form-input>
 
-           <b-input-group-prepend is-text>
+          <b-input-group-prepend is-text>
             <b-icon-search class="text-primary border-none"></b-icon-search>
           </b-input-group-prepend>
-
-
         </b-input-group>
       </b-col>
-
-
     </b-row>
-    
-    <br/>
 
+    <br />
 
+    <b-row>
+      <b-col md="12" lg="6">
+        <CommunityMembers />
+      </b-col>
 
-
-
-
-
-
-
-
-
-
-<b-row> 
-<b-col md="12" lg="6">  
-    <CommunityMembers/>
-
- </b-col>
-
-
- <b-col md="12" lg="6">  
-    <CommunityMembers/>
-
- </b-col>
-
-     </b-row>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
+      <b-col md="12" lg="6">
+        <CommunityMembers />
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
-import CommunityMembers from "../../communityMember"
+import CommunityMembers from "../../communityMember";
 export default {
-  components:{
-      CommunityMembers
-  }
-}
+  components: {
+    CommunityMembers,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>

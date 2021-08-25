579aa79bb938b3627885093004bc4d58d0d33c66
Sir-Deon <sird655@gmail.com>
2021-08-25 13:13:22 -0400

    finished business community

--- src/components/businessOwner/intro.vue
+++ src/components/businessOwner/intro.vue
@@ -1,24 +1,31 @@
 <template>
   <div>
     <b-card title="" header-tag="header" footer-tag="footer">
-
-       <div  style="float:right" class="edit" @click="editContact">    
-             <b-icon icon="pencil-fill" variant="primary"></b-icon>
-            </div>
+      <div style="float:right" class="edit" @click="editContact">
+        <b-icon icon="pencil-fill" variant="primary"></b-icon>
+      </div>
       <h6 class="mb-2"><b>Intro</b></h6>
 
       <b-card-text>
         <p>
-          <b-icon icon="briefcase-fill" class="primary icon-size"></b-icon> Agriculture
+          <b-icon icon="briefcase-fill" class="primary icon-size"></b-icon>
+          Agriculture
         </p>
-        <p><b-icon icon="search" class="primary icon-size"></b-icon> Chicken Seller</p>
         <p>
-          <b-icon icon="geo-alt-fill" class="primary icon-size"></b-icon> Mokolo, Yaounde,
-          Cameroon
+          <b-icon icon="search" class="primary icon-size"></b-icon> Chicken
+          Seller
         </p>
-        <p><b-icon icon="link" class="primary icon-size"></b-icon> www.business.com</p>
         <p>
-          <b-icon icon="people-fill" class="primary icon-size"></b-icon> 1.1M Community
+          <b-icon icon="geo-alt-fill" class="primary icon-size"></b-icon>
+          Mokolo, Yaounde, Cameroon
+        </p>
+        <p>
+          <b-icon icon="link" class="primary icon-size"></b-icon>
+          www.business.com
+        </p>
+        <p>
+          <b-icon icon="people-fill" class="primary icon-size"></b-icon> 1.1M
+          Community
         </p>
         <p>
           <b-icon icon="telephone-fill" class="primary icon-size"></b-icon>
@@ -41,34 +48,15 @@
         </p>
       </b-card-text>
 
-
-
-
-
-            <GmapMap
-      :center='center'
-      :zoom='12'
-      style='width:100%;  height: 250px;'
-    >
-      <GmapMarker
-        :key="index"
-        v-for="(m, index) in markers"
-        :position="m.position"
-        @click="center=m.position"
-      />
-    </GmapMap>
-
-
-
-
-
-
-
+      <GmapMap :center="center" :zoom="12" style="width:100%;  height: 250px;">
+        <GmapMarker
+          :key="index"
+          v-for="(m, index) in markers"
+          :position="m.position"
+          @click="center = m.position"
+        />
+      </GmapMap>
     </b-card>
-
-
-
-    
 
     <b-modal
       id="bv-modal-example2"
@@ -77,64 +65,52 @@
       v-model="edit2"
     >
       <b-form>
+        <div class="form-group">
+          <label for="username">Busness Name:</label><br />
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Busness Name"
+            class="form-control"
+          />
+        </div>
 
-        
-                      <div class="form-group">
-                        <label for="username">Busness Name:</label><br />
-                        <input
-                          type="text"
-                          name="username"
-                          id="username"
-                          placeholder="Busness Name"
-                          class="form-control"
-                        />
-                      </div>
+        <div class="form-group">
+          <label for="alias">Category:</label><br />
 
-
-
-
-                      
-                      <div class="form-group">
-                        <label for="alias">Category:</label><br />
-                        
-
-                         <b-form-select
+          <b-form-select
             v-model="category"
             :options="categories"
             class="mb-3"
             value-field="item"
             text-field="name"
           ></b-form-select>
+        </div>
 
-                      </div>
+        <div class="form-group">
+          <label for="username">Keywords</label><br />
+          <div class="col-md-12 pl-0 pr-0">
+            No Choices
 
-
-
-                      <div class="form-group">
-                        <label for="username">Keywords</label><br />
-                        <div class="col-md-12 pl-0 pr-0">
-                          No Choices
-
-                          <input
-                          type="text"
-                          name="alias"
-                          id="alias"
-                          placeholder="Enter your Keywords"
-                          class="form-control"
-                        />
-
-                      </div>
-                    </div>
+            <input
+              type="text"
+              name="alias"
+              id="alias"
+              placeholder="Enter your Keywords"
+              class="form-control"
+            />
+          </div>
+        </div>
         <b-form-group
           id="input-group-1"
-          label="Country"     
+          label="Country"
           label-for="input-1"
           label-size="sm"
         >
           <b-form-input
             id="input-1"
             class="mt-1"
-          
             type="text"
             required
           ></b-form-input>
@@ -148,7 +124,6 @@
           <b-form-input
             id="input-1"
             class="mt-1"
-           
             type="text"
             required
           ></b-form-input>
@@ -159,19 +134,14 @@
           label-for="input-2"
           label-size="sm"
         >
-
-
-
-         <b-form-input
+          <b-form-input
             id="input-1"
             class="mt-1"
-           
             type="text"
             required
           ></b-form-input>
-         
         </b-form-group>
-        
+
         <b-form-group
           id="input-group-2"
           label="Phone Contact"
@@ -209,26 +179,20 @@
         </b-button>
       </b-form>
     </b-modal>
-
-
   </div>
 </template>
 
 <script>
 export default {
-
-
   data() {
     return {
-
-       center: { lat: 45.508, lng: -73.587 },
+      center: { lat: 45.508, lng: -73.587 },
       currentPlace: null,
       markers: [],
       places: [],
 
-       category:'',
-       categories: [ 
-
+      category: "",
+      categories: [
         { item: "Professional_and_home_service", name: "Professionals" },
         { item: "Agriculture ", name: "Agriculture " },
         { item: "Restaurant ", name: " Restaurant " },
@@ -241,13 +205,11 @@ export default {
         { item: "Hair_and_beauty ", name: "Hair and beauty " },
         { item: "Real_estate ", name: "Real_estate " },
         { item: "Travelling ", name: "Travelling " },
-         { item: "Hotels", name: "Hotels" },
-          { item: "station", name: " station  " },
-           { item: "Mayor_concils", name: "Mayor_concils" },
-           { item: "Taxis service", name: "Taxis service" },
-
+        { item: "Hotels", name: "Hotels" },
+        { item: "station", name: " station  " },
+        { item: "Mayor_concils", name: "Mayor_concils" },
+        { item: "Taxis service", name: "Taxis service" },
       ],
-
 
       edit1: false,
       edit2: false,
@@ -269,7 +231,7 @@ export default {
       cityName: "Dummy",
       followed: "525",
       telephone: "+1 (234) 567-8974",
-      email: "info@businessname.com"
+      email: "info@businessname.com",
     };
   },
   methods: {
@@ -294,60 +256,38 @@ export default {
      */
     validate(idForm) {
       this.$bvModal.hide(idForm);
-    }
-  }
-
-
+    },
+  },
 };
 </script>
 
 <style scoped>
-
-
 @media (max-width: 768px) {
+  .primary {
+    margin-right: 6px;
+    font-size: 12px !important;
+  }
 
- .primary{
-            
-           margin-right: 6px;
-             font-size: 12px  !important;
-
-    }
-
-     .card-text{
-      font-size: 14px  !important;
-    }
-
-
-  
+  .card-text {
+    font-size: 14px !important;
+  }
 }
-
-
 
 @media (min-width: 768px) {
+  .primary {
+    margin-right: 8px;
+    font-size: 14px !important;
+  }
 
- .primary{
-            
-           margin-right: 8px;
-           font-size: 14px  !important;
-    }
-
-    .card-text{
-      font-size: 14px  !important;
-    }
-
-   
-
+  .card-text {
+    font-size: 14px !important;
+  }
 }
-
-
 </style>
 
-<style> 
-
- .icon-size{
-      width: 24px;
-      height:24px;
-    }
-      </style>
-    
-   
+<style>
+.icon-size {
+  width: 24px;
+  height: 24px;
+}
+</style>
--- src/components/businessOwner/tabs/businesses/tabs/followers.vue
+++ src/components/businessOwner/tabs/businesses/tabs/followers.vue
@@ -1,65 +1,76 @@
 <template>
   <div>
-           <b-row>
-
-           
-      <b-col cols="12" md="6" >
-
-
+    <b-row>
+      <b-col cols="12" md="6">
         <b-input-group class="mb-2 px-md-3 float-right">
-         
           <b-form-input
             aria-label="Text input with checkbox"
             placeholder="Search Something"
+            v-model="searchQuery"
           ></b-form-input>
 
-           <b-input-group-prepend is-text>
+          <b-input-group-prepend is-text>
             <b-icon-search class="text-primary border-none"></b-icon-search>
           </b-input-group-prepend>
-
-
         </b-input-group>
       </b-col>
-
-
     </b-row>
-    <br/>
+    <br />
 
-    <br/>
+    <br />
 
-
-
-  
-
-
-
-  <b-row> 
-<b-col md="12" lg="6">  
-    <CommunityBusiness/>
-
- </b-col>
-
-
-  <b-col md="12" lg="6">  
-   <CommunityBusiness/>
-
- </b-col>
-
-     </b-row>
-
-
+    <b-row>
+      <b-col
+        v-for="(follower, index) in theFollowers"
+        :key="index"
+        md="12"
+        lg="6"
+      >
+        <CommunityBusiness :follower="follower" />
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
-import CommunityBusiness from "../../communitybusiness"
+import axios from "axios";
+import CommunityBusiness from "../../communitybusiness";
 export default {
-  components:{
-      CommunityBusiness
-  }
-}
+  components: {
+    CommunityBusiness,
+  },
+  data: () => ({
+    followers: [],
+    searchQuery: "",
+  }),
+  computed: {
+    theFollowers() {
+      if (this.searchQuery) {
+        return this.followers.filter(item => {
+          return this.searchQuery
+            .toLowerCase()
+            .split(" ")
+            .every(v => item.name.toLowerCase().includes(v));
+        });
+      } else {
+        return this.followers;
+      }
+    },
+  },
+  beforeMount() {
+    axios.defaults.headers.common["Authorization"] =
+      "Bearer " + localStorage.getItem("access_token");
+    this.getFollowers();
+  },
+  methods: {
+    getFollowers() {
+      axios.get("/community/business-follower/5").then(res => {
+        console.log(res.data.data);
+        this.followers = res.data.data.data;
+      });
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>

5772621f3565a9032a49c09bd2d296c76ca1b5d5
TYPW1 <39987299+TYPW1@users.noreply.github.com>
2021-08-28 11:51:13 +0100

    dashboard (pop net)

--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,11 @@ import Vuex from "vuex";
 import axios from "axios";
 
 Vue.use(Vuex);
-axios.defaults.baseURL = process.env.VUE_APP_baseURL;
+//axios.defaults.baseURL = process.env.VUE_APP_baseURL;
+axios.defaults.baseURL = " https://580f-154-72-150-86.ngrok.io/api/v1";
 
 const getDefaultState = () => {
   return {
-    api_link:"https://94e9-154-72-150-118.ngrok.io/api/v1",
-    api_link_end:"/business/details",
-    token: "8|Yx3DU4s08aFTYOCa3T2XJKZkjJV4leSi9b20oo5D",
     bdetails: []
   };
 };
@@ -20,14 +18,10 @@ const state = getDefaultState();
 
 const actions = {
   getbdetails({ commit }) {
-    return axios
-      .get(state.api_link+state.api_link_end, {
-        headers: { Authorization: `Bearer ${state.token}` }
-      })
-      .then(function({ data }) {
-        commit("set_details", data.data);
-        console.log(data);
-      });
+    return axios.get("/business/details", {}).then(function({ data }) {
+      commit("set_details", data.data);
+      console.log(data);
+    });
   }
 };
 

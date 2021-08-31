5b6feb0563c2c1a771ebb684551143ac33c8712f
TYPW1 <39987299+TYPW1@users.noreply.github.com>
2021-08-31 21:18:52 +0100

    corrected index VUE_APP_API_URL

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ import networkDetails from "./networkDetails";
 import axios from "axios";
 
 Vue.use(Vuex);
-axios.defaults.baseURL = process.env.VUE_APP_baseURL;
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
 
 export default new Vuex.Store({
   modules: {

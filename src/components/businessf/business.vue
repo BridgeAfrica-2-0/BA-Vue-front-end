743e897649a75d47043825c96995350535efb87e
Sir-Deon <sird655@gmail.com>
2021-09-01 14:12:54 +0100

    worked on business community

--- src/components/businessf/business.vue
+++ src/components/businessf/business.vue
@@ -137,7 +137,7 @@ import HomePage from "../businessf/tabs/businessHome";
 import About from "./tabs/about";
 import Media from "./tabs/media";
 import MarketPlace from "./tabs/marketPlace";
-// import Community from "./tabs/memberNetwork";
+import Community from "./tabs/memberNetwork";
 import Networks from "./tabs/networks";
 
 export default {
@@ -148,6 +148,7 @@ export default {
     Media,
     MarketPlace,
     Networks,
+    Community,
   },
 
   data() {

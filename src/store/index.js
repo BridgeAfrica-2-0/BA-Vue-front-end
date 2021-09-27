c9d2ddb430e6d9224f85d2e12e625b6d513063b3
Carlewis <afreecarl@gmail.com>
2021-09-27 14:06:47 +0100

    2pm_commit_27/9/21

--- src/store/index.js
+++ src/store/index.js
@@ -14,7 +14,9 @@ import networkProfileFeedback  from "./networkProfileFeedback";
 
 import axios from "axios";
 Vue.use(Vuex);
- axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+//  axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+// axios.defaults.baseURL = process.env.VUE_APP_API_URL_DEV;
+ axios.defaults.baseURL = "https://edson.maxinemoffett.com/api/v1/";
 export default new Vuex.Store({
   modules: {
     auth,

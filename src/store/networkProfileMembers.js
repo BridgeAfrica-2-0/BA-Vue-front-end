4e8f1d44d98e3fa602a013c12c7f88841eaf7401
Carlewis <afreecarl@gmail.com>
2021-10-02 10:02:40 +0100

    10am_commit_2/10/21

--- src/store/networkProfileMembers.js
+++ src/store/networkProfileMembers.js
@@ -43,24 +43,7 @@ export default {
         console.log(data);
       })
     },
-    // getadmins( {commit}, data){
-    //   console.log('getadmins: '+data.path);
-    //   console.log('keyword: '+data.keyword);
-    //   console.log(data.keyword);
-    //   return axios
-    //   .post(`network/${data.path}`, {
-    //       headers: {
-    //         'content-type': 'application/json'
-    //       },
-    //       data: {
-    //           "keyword": data.keyword
-    //       }
-    //     })
-    //   .then(({ data }) => {
-    //       commit("setadmins", data.data);
-    //     console.log(data);
-    //   })
-    // },
+ 
     getadmins( {commit}, data){
       console.log('getadmins: '+data.path);
       console.log('keyword: '+data.keyword);
@@ -73,7 +56,7 @@ export default {
       })
     },
     getbusiness( {commit}, data ){
-        console.log('getadmins: '+data.path);
+        console.log('getbusiness: '+data.path);
         console.log('keyword: '+data.keyword);
       return axios
       .post(`network/${data.path}`, { "keyword": data.keyword})

35ca151ac1e631418308d2459435d27bb3ece448
Nji Daniel <njid18753@gmail.com>
2021-10-07 19:53:05 +0100

    Added screens for page 3 payment

---
+++ Page 3 Payment screen/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <Stepper/>
+
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+import Stepper from './components/Stepper.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+       Stepper
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+ 
+}
+</style>

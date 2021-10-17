819397ed00910c2635df2f95d041f996ba22146c
Divine16772 <truthyoung16@gmail.com>
2021-10-18 00:36:44 +0100

    revert baseURL

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,7 @@ import networkSetting from "./networkSetting";
 
 import axios from "axios";
 Vue.use(Vuex);
-axios.defaults.baseURL = process.env.VUE_APP_API_URL_DEV;
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
 
 export default new Vuex.Store({
   modules: {

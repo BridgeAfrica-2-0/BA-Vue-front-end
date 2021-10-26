ce1dd634d79944f875612057448f672618c53f1d
Carlewis <afreecarl@gmail.com>
2021-10-26 10:03:38 +0100

    10pm_commit_26/10/21

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,47 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import auth from "./auth";
+import businessBlocking from "./businessBlocking";
+import dashboard from "./dashboard";
 import businessOwner from "./businessOwner";
 import networkDetails from "./networkDetails";
 import dashboardcommunity from "./dashboardcommunity";
+import businessGeneral from "./businessGeneral";
+import businessRole from "./businessRole";
+import businessAccountType from "./businessAccountType";
 
+import NetworkSettings from "./NetworkSettings";
+import ProfileAndBusinessDetails from "./ProfileAndBusinessDetails";
+import businessFollowers from "./businessFollowers";
+import market from "./market";
+import hotbusiness from "./hotbusiness";
+import profile from "./profile";
+import follower from "./follower";
+import UserProfileOwner from "./UserProfileOwner"
 import axios from "axios";
+import { search } from "./search";
+
 Vue.use(Vuex);
-// axios.defaults.baseURL = process.env.VUE_APP_API_URL;
-axios.defaults.baseURL = process.env.VUE_APP_API_URL_ABU;
+ axios.defaults.baseURL = process.env.VUE_APP_API_URL; 
 
 export default new Vuex.Store({
   modules: {
     auth,
-    businessOwner,
     networkDetails,
-    dashboardcommunity
-  }
+    dashboardcommunity,
+    businessOwner,
+    businessBlocking,
+    dashboard,
+    businessGeneral,
+    market,
+    ProfileAndBusinessDetails,
+    hotbusiness,
+    businessRole,
+    businessAccountType,
+    NetworkSettings,
+    profile,
+    follower,
+    search,
+    UserProfileOwner
+  },
 });

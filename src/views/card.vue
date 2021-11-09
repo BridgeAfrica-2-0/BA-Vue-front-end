0170d9761224620a6eca908b4d0ef0d1ea4a4593
marcellin-dev <lelemarcellin@gmail.com>
2021-11-09 07:56:09 +0100

    end of desktop version

--- src/views/card.vue
+++ src/views/card.vue
@@ -14,51 +14,60 @@
           </div>
         </div>
         <hr />
+        <div v-for="i in 4" :key="i">
+          <div class="row ">
+            <div class="col-3">
+              <splide :options="{ rewind: true }" class="r-img">
+                <splide-slide cl v-for="(im, index) in img" :key="index">
+                  <img :src="img[index]" class="r-img" />
+                </splide-slide>
+              </splide>
+            </div>
 
-        <div class="row">
-          <div class="col-3">
-            <splide :options="{ rewind: true }" class="r-img">
-              <splide-slide cl v-for="(im, index) in img" :key="index">
-                <img :src="img[index]" class="r-img" />
-              </splide-slide>
-            </splide>
-          </div>
-
-          <div class="col-4 text-end text-start bold">
-            <div class="row "> 
-                <div class="col-5 p-0"> <h3>Name of item  :</h3> </div>
-                <div class="col"> <h3 class="">product name</h3> </div>
-                </div>
-
-                <div class="row"> 
-                    <div class="col-5 p-0"><h3>product ID :</h3></div>
-                    <div class="col"><h3 class="">8653536</h3> </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-5 p-0 ">
-                        <h3 class="">description:</h3> 
-                    </div>
-                    <div class="col">
-                        <h3 class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quibusdam consequuntur sequi itaque, nostrum odio nobis magnam distinction</h3>
-                    </div>
-                </div>
-            
-           
-          </div>
-          <div class="col bg-primary"> 
+            <div class="col-4 text-end text-start bold">
               <div class="row">
-                  <div class="col "><input type="number" class="numbersize"></div>
-                  <div class="col text-success p-0">5000 XAF</div>
-                  <div class="col p-0">10000 XAF</div>
-                  <div class="col p-0">tra</div>
+                <div class="col-5 p-0"><h3>Name of item :</h3></div>
+                <div class="col"><h3 class="h3">product name</h3></div>
               </div>
-          </div>
 
+              <div class="row">
+                <div class="col-5 p-0"><h3>product ID :</h3></div>
+                <div class="col"><h3 class="h3">8653536</h3></div>
+              </div>
+
+              <div class="row">
+                <div class="col-5 p-0">
+                  <h3 class="">description:</h3>
+                </div>
+                <div class="col">
+                  <h3 class="h3">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Maxime quibusdam consequuntur sequi itaque..
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div class="row line"></div>
+            <div class="col ">
+              <div class="row ">
+                <div class="col-4 p-0 bg-y">
+                  <input type="number" value="2" class="numbersize" />
+                </div>
+                <div class="col text-success p-0">5000 XAF</div>
+                <div class="col p-0 text-success  text-center">10000 XAF</div>
+                <div class="col-1 p-0">
+                  <fas-icon
+                    class="couleur search card"
+                    :icon="['fas', 'trash-alt']"
+                  />
+                </div>
+              </div>
+            </div>
+          </div> <hr>
         </div>
       </div>
-
-      <div class="col-3 color">
+      <div class=" line"></div>
+      <div class="col-3 color ">
         <h3 class="my-2">ORDER SUMMARY</h3>
         <hr />
         <div class="row">
@@ -94,21 +103,70 @@
 </template>
 <script>
 export default {
-    data() {
-       return{
-           img: ["http://urlr.me/YMQXD", "https://placekitten.com/400/300"],
-       } 
-    },
-}
+  data() {
+    return {
+      img: ["http://urlr.me/YMQXD", "https://placekitten.com/400/300"],
+    };
+  },
+};
 </script>
 <style scoped>
+
+@media only screen and (max-width: 1200px) {
+  .color {
+  background-color: #c5c5c546;
+  margin-top: 15px;
+  /* height: 500px; */
+}
+h3 {
+  font-size: 12px;
+  font-weight: bold;
+}
+
+.numbersize {
+  width: 40px;
+  height: 20px;
+}
+.line{
+  width: 100%;
+}
+
+.btncolor {
+  background-color: #e75c18;
+  /* margin-top: 10px;
+  height: 30px;
+  width: 120px; */
+  color: white;
+  line-height: 10px;
+}
+.btn1 {
+  background-color: #e75c18;
+  /* margin-top: 10px;
+  height: 30px; */
+
+  color: white;
+  line-height: 10px;
+}
+.couleur {
+  color: red;
+}
+
+}
+
+
+/* style for desktop ------------------------------------ */
+@media only screen and (min-width: 1200px) {
 h3 {
   font-size: 14px;
   font-weight: bold;
 }
+.h3 {
+  font-weight: normal;
+}
 .color {
   background-color: #c5c5c546;
   margin-top: 15px;
+  height: 500px;
 }
 .btncolor {
   background-color: #e75c18;
@@ -126,13 +184,17 @@ h3 {
   color: white;
   line-height: 10px;
 }
+.couleur {
+  color: red;
+}
 .r-img {
   border-radius: 5px;
   height: 150px;
   width: 190px;
 }
-.numbersize{
-    width: 40px;
-    height: 20px;
+.numbersize {
+  width: 40px;
+  height: 20px;
+}
 }
 </style>

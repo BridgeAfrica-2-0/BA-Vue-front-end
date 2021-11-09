cb40bed58203cec4bb66691831b7710196e937d8
marcellin-dev <lelemarcellin@gmail.com>
2021-11-09 10:57:17 +0100

    designing the mobile version

--- src/views/card.vue
+++ src/views/card.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="container">
+      <navbar />
     <div class="row">
-      <div class="col">
+      <div class="col-12 col-md-9">
         <hr />
         <div class="row">
           <div class="col"><h3 class="mx-5">PRODUCT DETAILS</h3></div>
           <div class="col">
-            <div class="row">
+            <div class="row desktop">
               <h3 class="mx-5">Quantity</h3>
               <h3 class="mx-5">price</h3>
               <h3 class="mx-5">Total</h3>
@@ -16,7 +17,7 @@
         <hr />
         <div v-for="i in 4" :key="i">
           <div class="row ">
-            <div class="col-3">
+            <div class="col-4 col-md-3">
               <splide :options="{ rewind: true }" class="r-img">
                 <splide-slide cl v-for="(im, index) in img" :key="index">
                   <img :src="img[index]" class="r-img" />
@@ -24,7 +25,7 @@
               </splide>
             </div>
 
-            <div class="col-4 text-end text-start bold">
+            <div class="col-8 col-md-4 text-end text-start bold ">
               <div class="row">
                 <div class="col-5 p-0"><h3>Name of item :</h3></div>
                 <div class="col"><h3 class="h3">product name</h3></div>
@@ -48,16 +49,16 @@
               </div>
             </div>
             <div class="row line"></div>
-            <div class="col ">
+            <div class="col marg5">
               <div class="row ">
-                <div class="col-4 p-0 bg-y">
-                  <input type="number" value="2" class="numbersize" />
+                <div class="col-4 p-0 bg-y ">
+                  <input type="number" value="2" class="numbersize marg1" />
                 </div>
-                <div class="col text-success p-0">5000 XAF</div>
-                <div class="col p-0 text-success  text-center">10000 XAF</div>
-                <div class="col-1 p-0">
+                <div class="col text-success p-0 "><h3 class="marg2">5000 XAF</h3></div>
+                <div class="col p-0 text-success  text-center"><h3 class="marg3">10000 XAF</h3></div>
+                <div class="col-1 p-0  ">
                   <fas-icon
-                    class="couleur search card"
+                    class="couleur search  marg4"
                     :icon="['fas', 'trash-alt']"
                   />
                 </div>
@@ -67,7 +68,7 @@
         </div>
       </div>
       <div class=" line"></div>
-      <div class="col-3 color ">
+      <div class="col-12 col-md-3 color">
         <h3 class="my-2">ORDER SUMMARY</h3>
         <hr />
         <div class="row">
@@ -102,7 +103,9 @@
   </div>
 </template>
 <script>
+import navbar from "@/components/navbar.vue";
 export default {
+  components : {navbar}, 
   data() {
     return {
       img: ["http://urlr.me/YMQXD", "https://placekitten.com/400/300"],
@@ -112,17 +115,40 @@ export default {
 </script>
 <style scoped>
 
-@media only screen and (max-width: 1200px) {
+@media only screen and (max-width: 1201px) {
+  .desktop{
+    display: none;
+  }
   .color {
   background-color: #c5c5c546;
   margin-top: 15px;
   /* height: 500px; */
+  
+}
+
+.marg1{
+  margin-left: 180px;
+}
+.marg2{
+   margin-left: 90px;
+   /* padding-left: 2px; */
+}
+.marg3{
+
+}
+.marg4{
+
+}
+.marg5{
+  margin-top: -20px ;
 }
 h3 {
   font-size: 12px;
   font-weight: bold;
 }
-
+.h3 {
+  font-weight: normal;
+}
 .numbersize {
   width: 40px;
   height: 20px;
@@ -149,6 +175,12 @@ h3 {
 }
 .couleur {
   color: red;
+}
+
+.r-img {
+  border-radius: 5px;
+  height: 150px;
+  
 }
 
 }

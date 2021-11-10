26662fd67af7f7a7ad648e5c67a20eed8906ce98
Priscille-Atsa <cecileatsa7101@gmail.com>
2021-11-10 16:25:16 +0100

    Translation of Network tabs and about view

--- src/locales/en/index.js
+++ src/locales/en/index.js
@@ -1,12 +1,13 @@
-
 //import en from './en.json'
 import auth from './auth.json'
+import network from './network.json'
 
 
 export const defaultLocale = 'en'
 
 export const en = {
- 
-  auth: auth,
-  
+
+    auth: auth,
+    network: network,
+
 }
---
+++ src/locales/fr/index.js
@@ -0,0 +1,13 @@
+//import en from './en.json'
+import auth from './auth.json'
+import network from './network.json'
+
+
+export const defaultLocale = 'fr'
+
+export const fr = {
+
+    auth: auth,
+    network: network,
+
+}

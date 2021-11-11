b2ca6307285b41c0cc3da78a6591947cb55890f1
HeAhCamerounToiAussiQuelleImage <beignetharicot@gmail.com>
2021-11-11 15:39:19 +0100

    Change config echo file

--- src/notification.js
+++ src/notification.js
@@ -1,9 +1,25 @@
-import Echo from 'laravel-echo';
-window.Pusher = require('pusher-js');
-window.Echo = new Echo({
-  broadcaster: 'pusher',
-  key: "22cdbfa66f6ef7bfe041",
-  cluster: "eu",
-  forceTLS: true
-});
+// import Echo from 'laravel-echo';
+// window.Pusher = require('pusher-js');
+// window.Echo = new Echo({
+//   broadcaster: 'pusher',
+//   key: "22cdbfa66f6ef7bfe041",
+//   cluster: "eu",
+//   forceTLS: true
+// });
 
+import Vue from 'vue'
+import VuePusher from 'vue-pusher'
+
+Vue.use(VuePusher, {
+  api_key: "22cdbfa66f6ef7bfe041",
+  options: {
+    cluster:  "eu",
+    encrypted: true,
+    authEndpoint: `${process.env.VUE_APP_API_URL}/api/broadcasting/auth`,
+    auth: {
+      headers: {
+        Authorization: `Bearer ${store.state.auth.token}`
+      }
+    }
+  }
+})

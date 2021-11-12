f8d63ff0d0b7217b4b3cd68e054ce60cc12ded43
Priscille-Atsa <cecileatsa7101@gmail.com>
2021-11-12 13:53:36 +0100

    Translation of Member Network

--- src/locales/en/index.js
+++ src/locales/en/index.js
@@ -1,4 +1,3 @@
-
 //import en from './en.json'
 import auth from './auth.json'
 import businessf from './businessf.json'
@@ -9,18 +8,20 @@ import passrecover from './passrecover.json'
 import welcome from './welcome.json'
 import dashboard from './dashboard.json'
 import profileowner from './profileowner.json'
+import membernet from './membernet.json'
 
 export const defaultLocale = 'en'
 
 export const en = {
- 
-  auth: auth,
-  businessf:businessf,
-  search: search,
+
+    auth: auth,
+    businessf: businessf,
+    search: search,
 
     verification: verification,
     passrecover: passrecover,
     dashboard: dashboard,
     profileowner: profileowner,
     welcome: welcome,
+    membernet: membernet,
 }
--- src/locales/fr/index.js
+++ src/locales/fr/index.js
@@ -1,16 +1,26 @@
-
-    //import fr from './fr.json'
+//import fr from './fr.json'
 import auth from './auth.json'
 import businessf from './businessf.json'
 import search from './search.json'
+import verification from './verification.json'
+import passrecover from './passrecover.json'
+import welcome from './welcome.json'
+import dashboard from './dashboard.json'
+import profileowner from './profileowner.json'
+import membernet from './membernet.json'
 
 export const defaultLocale = 'fr'
 
 export const fr = {
- 
-  auth: auth,
-  businessf:businessf,
-  search: search,
-  
+
+    auth: auth,
+    businessf: businessf,
+    search: search,
+    verification: verification,
+    passrecover: passrecover,
+    welcome: welcome,
+    dashboard: dashboard,
+    profileowner: profileowner,
+    membernet: membernet,
 
 }

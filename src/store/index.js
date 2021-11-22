1e5f968c670db05a657b4627e46fa824a60c32f6
blezour <blezourblec@gmail.com>
2021-11-22 15:52:42 +0100

    break poin incase something goes wrong

--- src/store/index.js
+++ src/store/index.js
@@ -42,6 +42,7 @@ import { notification } from "./notifications"
 
 Vue.use(Vuex);
  axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+ axios.defaults.baseURL = "https://test.maxinemoffett.com/api/v1/";
 
 export default new Vuex.Store({
   modules: {

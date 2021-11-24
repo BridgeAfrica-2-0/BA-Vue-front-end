a6ea54db862c9040da1761ae03c0131e56ac46fe
blezour <blezourblec@gmail.com>
2021-11-24 23:35:22 +0100

    auick changes

--- src/components/memberNetworkF/members.vue
+++ src/components/memberNetworkF/members.vue
@@ -8,7 +8,7 @@
           </b-input-group-prepend>
           <b-form-input
             aria-label="Text input with checkbox"
-            :placeholder="$t('network.Search_Something')"
+            placeholder="Search Something"
             type="text"
             class="form-control"
             v-model="searchTitle"
@@ -21,8 +21,7 @@
     <b-row class="mt-4">
       <b-col cols="12">
         <h6 class="font-weight-bolder">
-          {{ $t('network.Network_Admins') }} 
-          ({{nFormatter(admins.length)}})
+          Network Admins ({{nFormatter(admins.length)}})
         </h6>
         <hr width="100%" />
         <b-skeleton-wrapper :loading="loading">
@@ -51,8 +50,7 @@
               </p>
             </div>
           </div>
-          <div v-else>{{ $t('network.No_Result_On_Admins') }} 
-</div>
+          <div v-else>No Result On Admins</div>
         </b-skeleton-wrapper>
       </b-col>
     </b-row>
@@ -60,8 +58,7 @@
     <b-row class="mt-4">
       <b-col cols="12" >
         <h6 class="font-weight-bolder">
-          {{ $t('network.Bussiness') }} 
- ({{nFormatter(business.length)}})
+          Bussiness ({{nFormatter(business.length)}})
         </h6>
         <hr width="100%" />
         <b-skeleton-wrapper :loading="loading">
@@ -91,8 +88,7 @@
               </p>
             </div>
           </div>
-          <div v-else>{{ $t('network.No_Result_On_Networks') }} 
-</div>
+          <div v-else>No Result On Networks</div>
         </b-skeleton-wrapper>
       </b-col>
     </b-row>
@@ -100,8 +96,7 @@
     <b-row class="mt-4" >
       <b-col cols="12">
         <h6 class="font-weight-bolder">
-          {{ $t('network.All_Members') }} 
-({{nFormatter(members.length)}})
+          All Members ({{nFormatter(members.length)}})
         </h6>
         <hr width="100%" />
         <b-skeleton-wrapper :loading="loading">
@@ -132,10 +127,8 @@
       </b-col>
       <b-col col="12">
         <infinite-loading @infinite="infiniteHandler">
-          <div class="text-red" slot="no-more">{{ $t('network.No_More_Request') }} 
-</div>
-          <div class="text-red" slot="no-results">{{ $t('network.No_More_Request') }} 
-</div>
+          <div class="text-red" slot="no-more">No More Request</div>
+          <div class="text-red" slot="no-results">No More Request</div>
         </infinite-loading>
       </b-col>
     </b-row>

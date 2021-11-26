d969037be0351d71a01cab35477137da0cfbc6e7
iSarahSajjad <42732016+iSarahSajjad@users.noreply.github.com>
2021-11-26 11:59:39 +0500

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -43,9 +43,9 @@ import profileSettingsEdit from "./profileSettingsEdit"
 import { social } from './social';
 
 Vue.use(Vuex);
-//axios.defaults.baseURL = process.env.VUE_APP_API_URL_DEV;
+axios.defaults.baseURL = process.env.VUE_APP_API_URL_DEV;
 // axios.defaults.baseURL = "https://edson.maxinemoffett.com/api/v1/";
- axios.defaults.baseURL = process.env.VUE_APP_API_URL_EDS;
+ // axios.defaults.baseURL = process.env.VUE_APP_API_URL_EDS;
 
 
 export default new Vuex.Store({

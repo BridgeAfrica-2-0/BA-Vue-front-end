48e79e58259177a8ce25d184019af3d0ee68eba2
HeAhCamerounToiAussiQuelleImage <beignetharicot@gmail.com>
2021-12-03 11:40:32 +0100

    Solve bug comment

--- src/views/networkEditors.vue
+++ src/views/networkEditors.vue
@@ -47,7 +47,6 @@
       </div>
     </div>
     <Footer />
-
      </span>
   </div>
 </template>
--- src/views/networks.vue
+++ src/views/networks.vue
@@ -159,10 +159,8 @@ export default {
         this.isloaded = true;
       })
       .catch((error) => {
-        console.log({ error: error });
-        console.log(error.response.status);
         if (error.response.status == 404) {
-          //  this.$router.push({ name: "notFound" });
+          this.$router.push({ name: "notFound" });
         }
       });
   },

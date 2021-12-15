903cc46de29385532cfe6375cf238ced3a00ce44
marcellin-dev <lelemarcellin@gmail.com>
2021-12-15 06:30:22 +0100

    fix home navbar

--- src/views/home1.vue
+++ src/views/home1.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div>
-    <head>
+    <!-- <head>
       <meta charset="utf-8" />
       <meta content="width=device-width, initial-scale=1.0" name="viewport" />
 
@@ -9,25 +9,21 @@
       <meta content="" name="description" />
       <meta content="" name="keywords" />
 
-      <!-- Favicons -->
+      
       <link href="assets/img/favicon.png" rel="icon" />
       <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
 
-      <!-- Google Fonts -->
-      <!-- <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-        rel="stylesheet"
-      /> -->
-    </head>
+      
+    </head> -->
 
     <!-- ======= Header ======= -->
     
 
      <site-header class="topbar" />   
     <header id="header" class="fixed-topp">
-      <div class="container d-flex align-items-center">
-<!--       
-        <a href="index.html" class="logo mr-auto"
+      <div class=" d-flex align-items-center">
+      
+        <!-- <a href="index.html" class="logo mr-auto"
           ><img src="../assets/img/BAC-clear-dotted-logo.png" alt="" class="img-fluid b-logo"
         /></a> -->
 

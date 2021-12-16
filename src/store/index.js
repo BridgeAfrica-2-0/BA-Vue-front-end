5171a1ba9fea77d2caca48b1ba457fafce7d820d
Sango Pascal <sangopascal49@gmail.com>
2021-12-16 11:08:08 +0100

    pull

--- src/store/index.js
+++ src/store/index.js
@@ -52,7 +52,6 @@ import networkSetting from './networkSetting';
 
 Vue.use(Vuex);
 
-<<<<<<< HEAD
 //axios.defaults.baseURL = process.env.VUE_APP_API_URL;
 axios.defaults.baseURL = "http://courage4.maxinemoffett.com/api/v1/";
 
@@ -62,17 +61,6 @@ axios.defaults.baseURL = "http://courage4.maxinemoffett.com/api/v1/";
 
 
 //axios.defaults.baseURL = "http://edson.maxinemoffett.com/api/v1/";
-=======
-
-
-
- //axios.defaults.baseURL = "https://courage4.maxinemoffett.com/api/v1/";
-//axios.defaults.baseURL = process.env.VUE_APP_API_URL_EDS;
-axios.defaults.baseURL = process.env.VUE_APP_API_URL;
-
-
-
->>>>>>> main
 
 export default new Vuex.Store({
     modules: {

9a37092e4b8e50d83bbbce53af5de5820fab3c91
hamzaafzal9009 <hamzaafzal9009@gmail.com>
2021-12-16 18:25:23 +0500

    welcome business instance mapbox

---
+++ src/components/welcome/businessmap.vue
@@ -0,0 +1,33 @@
+<template>
+  <div style="width: 100%; height: 250px">
+    <MglMap
+      :accessToken="accessToken"
+      :mapStyle.sync="mapStyle"
+      :center="center"
+      :zoom="zoom"
+      style="width: 100%; height: 250px"
+    >
+      <MglMarker :coordinates="center" color="red" />
+    </MglMap>
+  </div>
+</template>
+<script>
+import { MglMap, MglMarker } from "vue-mapbox";
+export default {
+  props: ["center"],
+  components: {
+    MglMap,
+    MglMarker,
+  },
+  data() {
+    return {
+      accessToken: process.env.VUE_APP_MAPBOX_TOKEN,
+      mapStyle: "mapbox://styles/mapbox/streets-v11",
+      zoom: 12,
+    };
+  },
+};
+</script>
+<style scoped>
+@import url("https://api.tiles.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css");
+</style>

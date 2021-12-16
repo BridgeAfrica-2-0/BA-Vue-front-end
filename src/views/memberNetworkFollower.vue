844b33f6ca3dc4763e67443d844980f6f7a11db5
Sango Pascal <sangopascal49@gmail.com>
2021-12-16 16:27:59 +0100

    update

--- src/views/memberNetworkFollower.vue
+++ src/views/memberNetworkFollower.vue
@@ -17,16 +17,7 @@
           </b-tabs>
         </b-col>
       </b-row>
-    </b-container><p class="text-center">
-      <span class="display-inline">
-        <b-link @click="$i18n.locale = 'en'"> {{ $t("auth.english") }}</b-link>
-        <span class="vl"></span>
-        <b-link class="ml-2" @click="$i18n.locale = 'fr'">
-          {{ $t("auth.french") }}
-        </b-link>
-      </span>
-      Bridge Africa © 2021
-    </p>
+    </b-container>
     <Footer />
 
   </div>

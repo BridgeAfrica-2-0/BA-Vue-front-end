111898f7189ef7dac4cc60933e001a2dbbd4b91d
HeAhCamerounToiAussiQuelleImage <beignetharicot@gmail.com>
2021-12-16 08:01:59 +0100

    Fix merge bug

--- src/main.js
+++ src/main.js
@@ -179,7 +179,6 @@ Vue.use(VueAgile);
 import TextareaAutosize from 'vue-textarea-autosize'
 Vue.use(TextareaAutosize)
 
-
 import CoolLightBox from 'vue-cool-lightbox';
 import 'vue-cool-lightbox/dist/vue-cool-lightbox.min.css';
 

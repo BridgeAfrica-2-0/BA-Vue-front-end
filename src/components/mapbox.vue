afc43ce11ea81e1ff475c3e14d2f078393f5748f
hamzaafzal9009 <hamzaafzal9009@gmail.com>
2021-12-28 15:20:08 +0500

    business owner about map marker added

--- src/components/mapbox.vue
+++ src/components/mapbox.vue
@@ -3,21 +3,33 @@
     <MglMap
       :accessToken="accessToken"
       :mapStyle.sync="mapStyle"
-      :center="coordinates"
+      :center="[business.lng, business.lat]"
       :zoom="zoom"
       style="width: 100%; height: 250px"
     >
-      <MglMarker :coordinates="coordinates" color="red" />
+      <MglMarker :coordinates="[business.lng, business.lat]" color="red">
+        <MglPopup>
+          <div class="row">
+            <div class="px-4 py-2">
+              <p class="text-center mb-0">{{ business.name }}</p>
+              <p class="text-center mb-0">
+                {{ business.address }}
+              </p>
+            </div>
+          </div>
+        </MglPopup>
+      </MglMarker>
     </MglMap>
   </div>
 </template>
 <script>
-import { MglMap, MglMarker } from "vue-mapbox";
+import { MglMap, MglPopup, MglMarker } from "vue-mapbox";
 export default {
-  props: ["coordinates"],
+  props: ["business"],
   components: {
     MglMap,
     MglMarker,
+    MglPopup,
   },
   data() {
     return {

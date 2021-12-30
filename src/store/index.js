abb57cd93aa7d631322cc60e79694d8465273e50
Carlewis <afreecarl@gmail.com>
2021-12-30 21:06:17 +0100

    9pm_commit_30/12/21

--- src/store/index.js
+++ src/store/index.js
@@ -51,8 +51,7 @@ import networkSetting from './networkSetting';
 
 Vue.use(Vuex);
 
-// axios.defaults.baseURL = process.env.VUE_APP_API_URL;
-axios.defaults.baseURL = 'https://devone.maxinemoffett.com/api/v1/';
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
 
 export default new Vuex.Store({
     modules: {

54df57c9185a84c83afd6d4d39419413ba84638c
Sango Pascal <sangopascal49@gmail.com>
2022-01-04 09:52:15 +0100

    clean up

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -109,7 +109,7 @@
                 :minMatchingChars="0"
                 :maxMatches="10"
                 :serializer="(item) => item.name"
-                placeholder="Where"
+                :placeholder="$t('general.Where')"
                 class="search-hh w-44"
               />
 

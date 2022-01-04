6d510245f8aff2455f84315bc7256e3442c1f733
hamzaafzal9009 <hamzaafzal9009@gmail.com>
2022-01-04 14:53:52 +0500

    tutorial section fixed

--- src/components/dasboard/tutorial.vue
+++ src/components/dasboard/tutorial.vue
@@ -2,15 +2,19 @@
   <div>
     <b-card class=" border shadow ">
       <h6 class="title">
-
-        <fas-icon class="icons" :icon="['fas', 'video']" /> <b> {{ $t('dashboard.Tutorial') }}</b>
-
+        <fas-icon class="icons" :icon="['fas', 'video']" />
+        <b> {{ $t("dashboard.Tutorial") }}</b>
       </h6>
 
       <div class="s-card">
         <b-row>
-          <b-col cols="6" class="m-media" v-for="post in posts" :key="post.id">
-            <div class="container-fluid  " v-if="post.video">
+          <b-col
+            cols="6"
+            class="m-media px-0 px-md-2"
+            v-for="post in posts"
+            :key="post.id"
+          >
+            <div class="container-fluid px-1 px-md-2" v-if="post.video">
               <b-embed
                 type="iframe"
                 aspect="16by9"
@@ -19,10 +23,8 @@
                 class="s-embed"
               ></b-embed>
               <p class="text">
-
-                {{ $t('dashboard.video_name') }} <br />
-                {{ $t('dashboard.video_description') }}
-
+                {{ $t("dashboard.video_name") }} <br />
+                {{ $t("dashboard.video_description") }}
               </p>
             </div>
           </b-col>
@@ -42,7 +44,7 @@ export default {
           video: "https://www.youtube.com/embed/JKJECtmqviQ",
           title: "Title 1",
           text:
-            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quisquam sequi, ullam aliquam ab illo suscipit, earum quam, doloribus id sit consequuntur tempora molestiae blanditiis."
+            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quisquam sequi, ullam aliquam ab illo suscipit, earum quam, doloribus id sit consequuntur tempora molestiae blanditiis.",
         },
 
         {
@@ -50,7 +52,7 @@ export default {
           video: "https://www.youtube.com/embed/xVISKL18jLs",
           title: "Title 1",
           text:
-            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quisquam sequi, ullam aliquam ab illo suscipit, earum quam, doloribus id sit consequuntur tempora molestiae blanditiis."
+            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quisquam sequi, ullam aliquam ab illo suscipit, earum quam, doloribus id sit consequuntur tempora molestiae blanditiis.",
         },
 
         {
@@ -58,7 +60,7 @@ export default {
           video: "https://www.youtube.com/embed/nMBQrM1FHZw",
           title: "Title 1",
           text:
-            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quisquam sequi, ullam aliquam ab illo suscipit, earum quam, doloribus id sit consequuntur tempora molestiae blanditiis."
+            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quisquam sequi, ullam aliquam ab illo suscipit, earum quam, doloribus id sit consequuntur tempora molestiae blanditiis.",
         },
 
         {
@@ -66,16 +68,16 @@ export default {
           video: "https://www.youtube.com/embed/iPhGrDZhaAU",
           title: "Title 1",
           text:
-            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quisquam sequi, ullam aliquam ab illo suscipit, earum quam, doloribus id sit consequuntur tempora molestiae blanditiis."
+            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quisquam sequi, ullam aliquam ab illo suscipit, earum quam, doloribus id sit consequuntur tempora molestiae blanditiis.",
         },
         {
           id: 7,
           image: "https://picsum.photos/300/150/?image=41",
           title: "Title 2",
           text:
-            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quisquam sequi, ullam aliquam ab illo suscipit, earum quam, doloribus id sit consequuntur tempora molestiae blanditiis."
-        }
-      ]
+            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quisquam sequi, ullam aliquam ab illo suscipit, earum quam, doloribus id sit consequuntur tempora molestiae blanditiis.",
+        },
+      ],
     };
   },
   methods: {
@@ -97,8 +99,8 @@ export default {
       if (index > -1) {
         this.posts.splice(index, 1);
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -124,7 +126,8 @@ export default {
 }
 
 .embed-responsive-16by9 {
-  height: 200px;
+  width: 100%;
+  height: 125px;
 }
 .s-embed {
   border-radius: 10px;

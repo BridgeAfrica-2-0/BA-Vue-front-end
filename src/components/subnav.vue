c8c40e72951f85974fbfb1ca979522f403eb6c55
Edouard Yonga <ededwardyonga@gmail.com>
2022-01-13 11:32:53 +0100

    uptodate code

--- src/components/subnav.vue
+++ src/components/subnav.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="container-flex mobi d-none d-lg-none d-xl-block search">
-
-  
     <b-row>
       <b-col cols="1" lg="2"> </b-col>
       <b-col cols="8" lg="10" style="text-align: left">
@@ -10,21 +8,23 @@
             v-for="(category, index) in categories.slice(0, 6)"
             :key="index"
             @mouseover="showSubCat(category.category.id, category.sub_cat)"
-            @click="showSubCat(category.category.id, category.sub_cat)"
             @mouseleave="hideSubCat(category.category.id)"
           >
             <b-nav-item-dropdown
-              id="dropdown-1"
-              :text="category.category.name"
+              :id="'dropdown-' + index"
               :ref="category.category.id"
-              @click="searchProduct({ cat_id: category.category.id })"
             >
+              <template slot="button-content">
+                <span @click="bcategory({ cat_id: category.category.id })">
+                  {{ category.category.name }}
+                </span>
+              </template>
               <hr
                 style="
                   margin-top: -10px;
                   background-color: red;
                   height: 3px;
-                  width: 40%;
+                  width: 41%;
                   float: left;
                 "
               />
@@ -34,7 +34,7 @@
                   <b-dropdown-item
                     v-for="(subCat, subIndex) in category.sub_cat.slice(0, 6)"
                     :key="subIndex"
-                    @click="bcategory(subCat)"
+                    @click="bcategory({ cat_id: subCat.cat_id, id: subCat.id })"
                     href="#"
                     class="ml-2"
                   >
@@ -81,7 +81,10 @@
                   :key="index"
                   class="padding:50px"
                 >
-                  <b-dropdown-item class="ml-1" @click="getProducts()">
+                  <b-dropdown-item
+                    class="ml-1"
+                    @click="bcategory({ cat_id: category.category.id })"
+                  >
                     {{ category.category.name }}
                   </b-dropdown-item>
                 </b-col>
@@ -89,8 +92,6 @@
             </div>
           </b-nav-item-dropdown>
         </span>
-
-        <div></div>
       </b-col>
 
       <b-col cols="1" lg="2"> </b-col>
@@ -115,13 +116,12 @@ export default {
   created() {
     this.getCategories();
   },
-
   methods: {
     bcategory(category) {
+      console.log("HOVER...", category);
       this.$emit("category", category);
-      console.log(category);
+      // console.log(category);
     },
-
     getCategories() {
       this.$store
         .dispatch("marketSearch/getCategories")
@@ -141,24 +141,36 @@ export default {
       await this.$store.dispatch("marketSearch/searchProducts", data);
       // console.log("PRoducts ", this.$store.getters["marketSearch/getProducts"]);
     },
-
+    searchByCat(data) {
+      console.log("nani...");
+      this.allSearch(data);
+      this.searchProduct(data);
+    },
+    allSearch(data) {
+      console.log("[data]: ", data);
+      this.$store
+        .dispatch("allSearch/SEARCH", data)
+        .then((res) => {
+          // console.log("categories loaded!");
+        })
+        .catch((err) => {
+          console.log("Error erro!");
+        });
+    },
     showSubCat(catId, subCat) {
       this.$refs[catId][0].visible = true;
       this.$emit("parentcategory", catId);
       // this.subCategories.push(subCat);
-
       // this.searchProduct({ catId: catId, cat_id: catId });
       this.$store.commit("marketSearch/setSubCat", subCat);
       if (!subCat.length) this.hideSubCat(catId);
-      console.log("Subcat:", this.subCategories);
+      // console.log("Subcat:", this.subCategories);
     },
     hideSubCat(catId) {
       this.$refs[catId][0].visible = false;
       this.subCategories = [];
     },
-
     // ------------------------------------
-
     onOverMore() {
       this.$refs.More.visible = true;
       this.$emit("parentcategory", "More");
@@ -174,22 +186,18 @@ export default {
 .logo-img {
   width: 25px;
 }
-
 .mgmg {
   margin-left: 30px;
 }
-
 @media only screen and (max-width: 768px) {
   .mobi {
     display: none;
   }
 }
-
 .a-center {
   align-content: center;
   text-align: center;
 }
-
 .drop-text {
   color: black;
 }

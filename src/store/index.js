4546ec31e4578aed1322f28826908493fe0f8c49
Arman Sheikh <armansheikh92@gmail.com>
2022-01-18 14:04:52 +0500

    updated base url and install pusher js

--- src/store/index.js
+++ src/store/index.js
@@ -53,8 +53,8 @@ import productDetails from './productDetails';
 Vue.use(Vuex);
 
 
-// axios.defaults.baseURL = process.env.VUE_APP_API_URL;
-axios.defaults.baseURL = "https://edson.maxinemoffett.com/api/v1"
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+//axios.defaults.baseURL = "https://edson.maxinemoffett.com/api/v1"
 
 
 export default new Vuex.Store({

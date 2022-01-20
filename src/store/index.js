a5a23ddc918b6bc9ba58a843246141347d77d381
HeAhCamerounToiAussiQuelleImage <beignetharicot@gmail.com>
2022-01-20 16:32:45 +0100

    Add resize

--- src/store/index.js
+++ src/store/index.js
@@ -52,10 +52,8 @@ import networkSetting from './networkSetting';
 import productDetails from './productDetails';
 Vue.use(Vuex);
 
-
-
 axios.defaults.baseURL = process.env.VUE_APP_API_URL;
-
+axios.defaults.baseURL = "https://courage4.maxinemoffett.com/api/v1/";
 
 
 export default new Vuex.Store({

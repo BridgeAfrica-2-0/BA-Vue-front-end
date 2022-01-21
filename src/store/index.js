76c0108696eea5a3689cd544ccd0a478461a0625
HeAhCamerounToiAussiQuelleImage <beignetharicot@gmail.com>
2022-01-21 17:24:52 +0100

    Add prod api link

--- src/store/index.js
+++ src/store/index.js
@@ -53,7 +53,7 @@ import productDetails from './productDetails';
 Vue.use(Vuex);
 
 axios.defaults.baseURL = process.env.VUE_APP_API_URL;
-axios.defaults.baseURL = "https://courage4.maxinemoffett.com/api/v1/";
+//axios.defaults.baseURL = "https://courage4.maxinemoffett.com/api/v1/";
 
 
 export default new Vuex.Store({

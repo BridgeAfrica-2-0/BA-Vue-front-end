0f5566b3f35d27c601b7d9e4f322ddff65f8f0ed
Hafiz Furqan <hafiz.furqan@alphasquared.co>
2022-02-23 23:43:09 +0500

    safari optimization

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,10 +1,43 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
+  mode: 'production',
+  optimization: {
+    nodeEnv: 'production',
+    minimize: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s|css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
   pluginOptions: {
     i18n: {
       locale: "en",
       fallbackLocale: "en",
       localeDir: "locales",
-      enableInSFC: true
+      enableInSFC: false
     }
-  }
+  },
+  transpileDependencies: [
+    // can be string or regex
+    'vue-moment',
+    'vue-mapbox',
+    'vue-animate-onscroll',
+    'vue-agile',
+    'mapbox-gl'
+  ],
+  plugins: [
+    require('cssnano')({
+      preset: 'default'
+    }),
+    new MiniCssExtractPlugin({ filename: "[name].[hash].css" })
+  ]
 };

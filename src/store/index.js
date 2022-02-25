844202186f21f8d0c66b3c03bcb4b297e9c24173
HeAhCamerounToiAussiQuelleImage <beignetharicot@gmail.com>
2022-02-25 18:37:49 +0100

    Add prod link

--- src/store/index.js
+++ src/store/index.js
@@ -53,8 +53,8 @@ import businessGuest from './businessGuest';
 
 Vue.use(Vuex);
 
-//axios.defaults.baseURL = process.env.VUE_APP_API_URL;
-axios.defaults.baseURL = "https://courage4.maxinemoffett.com/api/v1/";
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+//axios.defaults.baseURL = "https://courage4.maxinemoffett.com/api/v1/";
 
 export default new Vuex.Store({
     modules: {

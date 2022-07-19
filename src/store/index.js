a78e7fca1290ca258e53d2726f01563fbfb0c719
blezour <blezourblec@gmail.com>
2022-07-19 16:24:37 +0200

    homepage quick fixed

--- src/store/index.js
+++ src/store/index.js
@@ -55,7 +55,8 @@ import businessGuest from './businessGuest';
 
 Vue.use(Vuex);
 
-axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+//axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+axios.defaults.baseURL = "http://courage4.maxinemoffett.com/api/v1/";
 
 export default new Vuex.Store({
     modules: {

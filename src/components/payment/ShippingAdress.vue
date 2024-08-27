ea998a4ccaf8ad38966a10bb10f6c3647ce3a2dd
farhan-faqir-esipick <farhan.faqir@esipick.com>
2024-08-27 13:46:25 +0500

    shipping form fixes and shipping address dropdown

--- src/components/payment/ShippingAdress.vue
+++ src/components/payment/ShippingAdress.vue
@@ -12,93 +12,27 @@
 				<CreateShippingModal />
 			</div>
 			<div class="col-12">
-				<CreateShippingModal
-					:title="$t('general.Edit_Shipping_Address')"
-					mode="edit"
-					:editForm="shippingsTab[0]"
-				/>
+				<CreateShippingModal :title="$t('general.Edit_Shipping_Address')" mode="edit"
+					:editForm="shippingsTab[0]" />
 			</div>
 			<div class="col-12">
-				<ChangeShippingAddress
-					:shippingsTab="shippingsTab"
-					:currentShipping="1"
-				/>
+				<ChangeShippingAddress :shippingsTab="shippingsTab" :currentShipping="1" />
 			</div>
 		</div>
 		<b-card-text class="mt-4 mr-0 w-100 d-flex justify-content-between align-items-start">
 			<div class="row w-100">
-				<div
-					class="ship-add w-100 col-12 d-flex justify-content-between align-items-start"
-					v-for="shipping_item in shippingsTab"
-					:key="shipping_item.id"
-				>  <div class="d-inline-flex">
+				<div class="col-12">
+					<b-form-select v-model="selectedShipping" @change="shipping(selectedShipping)">
+	<b-form-select-option v-for="shipping_item in shippingsTab" :key="shipping_item.id"
+		:value="shipping_item.id">
+		{{ shipping_item.name }}, {{ shipping_item.phone }}, {{
+			shipping_item.country }}, {{ shipping_item.email }}, {{
+			shipping_item.region }}, {{ shipping_item.city }}
+	</b-form-select-option>
+</b-form-select>
 
-					
-                        <div class="col-1">
 
-				
-	   <input type="radio" :v-model="shipping_item.id"  @change="shipping(shipping_item)"   :checked="shipping_item.active==1"     name="shipping" value="">   </div>
-	
 
-	      <div>   
-			<p class="body-font-size"> 
-							{{ shipping_item.name }}, {{ shipping_item.phone }},{{
-								shipping_item.country
-							}}, {{shipping_item.email}}
-						</p>
-						<p class="body-font-size">
-							{{ shipping_item.region }},{{ shipping_item.city }}
-							
-						</p>   </div>
-					</div>
-					<div class="">
-						<div class="">
-							<a
-								href="#"
-								v-b-modal.edit-shipping-modal
-								class="text-primary text-14 font-weight-bold text-uppercase mr-1 mr-sm-2"
-								>{{$t("general.Edit")}}</a
-							>
-							<div class="d-inline-block">
-								<b-dropdown
-									
-									right
-									variant="link"
-									toggle-class="text-decoration-none"
-									no-caret
-								>
-									<template #button-content>
-										<b-icon icon="three-dots-vertical"></b-icon>
-									</template>
-									<b-dropdown-item-button v-b-modal.create-shipping-modal>
-										<b-icon
-											variant="primary"
-											class="mr-2 icon-size"
-											icon="plus"
-											aria-hidden="true"
-										></b-icon>
-										<span class="">{{$t("general.Add")}}</span>
-									</b-dropdown-item-button>
-									<b-dropdown-item-button v-b-modal.change-shipping-modal>
-										<b-icon
-											variant="primary"
-											class="mr-2 icon-size"
-											icon="arrow-down-up"
-											aria-hidden="true"
-										></b-icon>
-										<span class="">{{$t("general.Change")}}</span>
-									</b-dropdown-item-button>
-									<ConfirmOperation
-										:message="$t('general.Do_you_want_to_delete_this_shipping_address')"
-										@sendid="handleDeleteShipping"
-										:id_item="shipping_item.id"
-									/>
-								</b-dropdown>
-							</div>
-						</div>
-					</div>
-
-				
 				</div>
 			</div>
 		</b-card-text>
@@ -111,131 +45,141 @@
 </template>
 
 <script>
-	import ConfirmOperation from "./ConfirmOperation.vue";
-	import CreateShippingModal from "./CreateShippingModal.vue";
-	import ChangeShippingAddress from "./ChangeShippingAddress.vue";
-	export default {
-		name: "ShippingAddress",
-		data() {
-			return {
-				loading: false,
-			};
-		},
-		components: {
-			ConfirmOperation,
-			CreateShippingModal,
-			ChangeShippingAddress,
-		},
-
-		
-		methods: {
+import ConfirmOperation from "./ConfirmOperation.vue";
+import CreateShippingModal from "./CreateShippingModal.vue";
+import ChangeShippingAddress from "./ChangeShippingAddress.vue";
+export default {
+	name: "ShippingAddress",
+	data() {
+		return {
+			loading: false,
+		};
+	},
+	components: {
+		CreateShippingModal,
+		ChangeShippingAddress,
+	},
 
 
-			shipping(data){
-             
-			 	let loader = this.$loading.show({
-        container: this.fullPage ? null : this.$refs.preview,
-        canCancel: true,
-        onCancel: this.onCancel,
-        color: "#e75c18",
-      });
+	methods: {
 
 
-			 this.$store.dispatch("checkout/choseShipping", data).then(() => {
+		shipping(data) {
+
+			let loader = this.$loading.show({
+				container: this.fullPage ? null : this.$refs.preview,
+				canCancel: true,
+				onCancel: this.onCancel,
+				color: "#e75c18",
+			});
+
+
+			this.$store.dispatch("checkout/choseShipping", data).then(() => {
 
 				this.$emit('RefreshSipping');
-				 loader.hide();
+				loader.hide();
 
-					  this.$store.dispatch("checkout/getCartt").then(() => {
-					     loader.hide();
+				this.$store.dispatch("checkout/getCartt").then(() => {
+					loader.hide();
 
-						 })
-				.catch(() => {
-					
-				});
-					
 				})
+					.catch(() => {
+
+					});
+
+			})
 				.catch(() => {
-					
+
 				});
 
-			
-			},
+
+		},
 
 
-			handleDeleteShipping(id) {
-
-			
-				this.$store.dispatch("checkout/deleteShippingAdd", id)
-
-				
-				
+		handleDeleteShipping(id) {
 
 
-			},
-			showConfirmModal() {
-				this.$emit("showconfirm");
-			},
+			this.$store.dispatch("checkout/deleteShippingAdd", id)
 
-			loadActualComponent3() {
-				this.$emit('loadActualComponent3')
-			},
 
-			loadActualComponent1() {
-				this.$emit('loadActualComponent1')
+
+
+
+		},
+		showConfirmModal() {
+			this.$emit("showconfirm");
+		},
+
+		loadActualComponent3() {
+			this.$emit('loadActualComponent3')
+		},
+
+		loadActualComponent1() {
+			this.$emit('loadActualComponent1')
+		}
+	},
+	computed: {
+		shippingsTab() {
+			console.log(this.$store.state.checkout.allShipping);
+			return this.$store.state.checkout.allShipping;
+
+		},
+		selectedShipping: {
+				get() {
+					// Find the active shipping item and return its ID
+					const activeShipping = this.shippingsTab.find(item => item.active == 1);
+					return activeShipping ? activeShipping.id : null;
+				},
+				set(value) {
+					// Allow v-model to set the selectedShipping value
+					this.selectedShipping = value;
+				}
 			}
-		},
-		computed: {
-			shippingsTab() {
-				console.log(this.$store.state.checkout.allShipping);
-				return this.$store.state.checkout.allShipping;
-				
-			},
-		},
-		mounted() {
-			this.loading = true;
-			this.$store
-				.dispatch("checkout/getAllShippingAdd")
-				.then(() => {
-					this.loading = false;
-				})
-				.catch(() => {
-					this.loading = false;
-				});
-		},
-	};
+	},
+	mounted() {
+		this.loading = true;
+		this.$store
+			.dispatch("checkout/getAllShippingAdd")
+			.then(() => {
+				this.loading = false;
+			})
+			.catch(() => {
+				this.loading = false;
+			});
+	},
+};
 </script>
 
 <style scoped>
-	/* .ship-add:not(:last-child){
+/* .ship-add:not(:last-child){
 	border-bottom: 1px solid #c2c0c0;
 } */
 
-.btnx{
+.btnx {
 	/* display: inline-block; */
-    font-weight: 400;
-    color: white;
+	font-weight: 400;
+	color: white;
 	background-color: #E75C18;
-    text-align: center;
-    vertical-align: middle;
+	text-align: center;
+	vertical-align: middle;
 	align-items: center;
 	text-transform: capitalize;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    user-select: none;
-    border: 1px solid transparent;
-    padding: 0.375rem 0.75rem;
-    font-size: 1rem;
-    line-height: 1.5;
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	user-select: none;
+	border: 1px solid transparent;
+	padding: 0.375rem 0.75rem;
+	font-size: 1rem;
+	line-height: 1.5;
 	float: right;
-    border-radius: 0.25rem;
+	border-radius: 0.25rem;
 	transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 }
 
 .btnx:hover {
 	color: #fff;
-    background-color: #cc4d0d;
-    border-color: #cc4d0d;
+	background-color: #cc4d0d;
+	border-color: #cc4d0d;
 }
 
 .headline_and_btns {
@@ -244,10 +188,10 @@
 }
 
 @media screen and (max-width: 600px) {
-    .headline_and_btns {
-        flex-direction: column;
+	.headline_and_btns {
+		flex-direction: column;
 		align-items: flex-start;
-    }
+	}
 
 	.buttons {
 		margin-top: 7px;
@@ -256,7 +200,9 @@
 }
 
 @media screen and (max-width: 376px) {
-    .btnx, .backBtn {
+
+	.btnx,
+	.backBtn {
 		float: left;
 	}
 

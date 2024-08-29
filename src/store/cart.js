94502505e4fe36f357e33581670b93ad50f09468
kashan ghori <64289221+khanakhun@users.noreply.github.com>
2024-08-29 19:32:01 +0500

    added cookies to guest requests

--- src/store/cart.js
+++ src/store/cart.js
@@ -1,30 +1,38 @@
 import axios from "axios";
-
 export default {
   namespaced: true,
 
   state: {
     cart: [],
-    status: ""
+    status: "",
   },
 
   mutations: {
     setStatus(state, status) {
       state.status = status;
-    }
+    },
   },
   actions: {
     async addToCart({ commit }, { product, islogin }) {
       const url = islogin
         ? `market/product/${product.id}/cart/add?business_id=${product.business_id}`
         : `guest/cart/product/${product.id}/cart/add?business_id=${product.business_id}`;
+      let options = islogin ? {} : { withCredentials: true };
       return await axios
-        .post(url)
-        .then(data => {
+        .post(url, {}, options)
+        .then((data) => {
           console.log(data);
+          const setCookieHeader = data.headers["set-cookie"]; // Correct header key for cookies
+
+          // Log the cookie value if it exists
+          if (setCookieHeader) {
+            console.log("Cookie received:", setCookieHeader);
+          } else {
+            console.log("No Set-Cookie header received");
+          }
           commit("setStatus", data.data.message);
         })
-        .catch(error => {
+        .catch((error) => {
           console.log({ error: error });
 
           if (error.response.status == 403) {
@@ -33,6 +41,6 @@ export default {
             commit("setStatus", "Can not add prodct to cart");
           }
         });
-    }
-  }
+    },
+  },
 };

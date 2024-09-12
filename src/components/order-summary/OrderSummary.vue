b4b73fcb4b2e358b542a5df0999295a0609b1e3d
farhan-faqir-esipick <farhan.faqir@esipick.com>
2024-09-12 22:14:00 +0500

    error fixes

--- src/components/order-summary/OrderSummary.vue
+++ src/components/order-summary/OrderSummary.vue
@@ -3,33 +3,32 @@
   <div class="order-summary">
     <div class="summary-section">
       <h3>Order Summary</h3>
-      <!-- <div class="summary-item">
+      <div class="summary-item">
         <span>Subtotal</span>
-        <span>${{ cartSummary?.sub_total.toFixed(2) ?? "" }}</span>
+        <span>${{ orderSummary?.total_items.toFixed(2) ?? "" }}</span>
       </div>
       <div class="summary-item">
         <span>Local Shipping</span>
-
-        <span>{{ cartSummary?.shipping ?? "" }}</span>
+       
+        <span>{{ orderSummary?.shipping ?? "" }}</span>
       </div>
       <div class="summary-item">
         <span>Estimated Tax</span>
-        <span>${{ cartSummary?.tax.toFixed(2) ?? 0.0 }}</span>
+        <span>${{ orderSummary?.tax.toFixed(2) ?? 0.0 }}</span>
       </div>
       <hr />
       <div class="summary-item total">
         <span>Total</span>
-        <span>${{ cartSummary?.total_cost.toFixed(2) ?? "" }}</span>
+        <span>${{ orderSummary?.total_cost.toFixed(2) ?? "" }}</span>
       </div>
       <hr />
       <p class="discount">
-        You will save ${{ cartSummary?.discount.toFixed(2) ?? "" }} with this
-        order
-      </p> -->
+        You will save ${{ orderSummary?.discount.toFixed(2) ?? "" }} with this order
+      </p>
     </div>
     <div>
       <button @click="submitOrder" class="submit-order-btn">
-        {{ getButtonText }}
+        Submit your order
       </button>
       <small>
         By clicking Submit Your Order, you are agreeing to our Terms of use and
@@ -59,59 +58,21 @@
 import { mapGetters } from "vuex";
 
 export default {
-  data() {
-    return {
-      cartSummary: {
-        total_items: 0,
-        shipping: "FREE",
-        tax: 0.0,
-        total_cost: 0.0,
-        sub_total: 0.0,
-        discount: 0.0,
-      },
-      currentStep: 0,
-    };
-  },
   computed: {
     ...mapGetters({
       orderSummary: "checkout/getCartSummary",
-      getCurrentStep: "checkout/getCurrentStep",
     }),
-    getButtonText() {
-      switch (this.currentStep) {
-        case 0:
-          return "Submit your order";
-        case 1:
-          return "Continue to payment";
-        case 2:
-          return "Confirm order";
-        default:
-          return "Submit your order";
-      }
-    },
   },
   methods: {
     submitOrder() {
+      // Handle order submission logic here
+      console.log("Order submitted:", this.orderSummary);
       this.$router.push("/checkout");
       // this.$emit("showoperator");
     },
   },
   mounted() {
-    if (this.orderSummary) {
-      this.cartSummary = { ...this.cartSummary, ...this.orderSummary };
-    }
-  },
-  watch: {
-    orderSummary(newVal) {
-      if (newVal) {
-        this.cartSummary = { ...this.cartSummary, ...newVal };
-      }
-    },
-    getCurrentStep(newVal) {
-      if (newVal) {
-        this.currentStep = newVal;
-      }
-    },
+    console.log("order summary", this.orderSummary);
   },
 };
 </script>
@@ -168,7 +129,7 @@ export default {
 }
 .help-section h4 {
   font-weight: bolder;
-  margin-bottom: 20px;
+  margin-bottom: 20px
 }
 .help-item {
   display: flex;

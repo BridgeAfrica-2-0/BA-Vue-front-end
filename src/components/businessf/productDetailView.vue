a36e7a51798693a7a0924066c1a203faa9750ee1
sanaiqbal5 <sana.iqbal.siqbal@gmail.com>
2025-04-24 11:55:32 +0500

    added static delivery delay to product descriptions

--- src/components/businessf/productDetailView.vue
+++ src/components/businessf/productDetailView.vue
@@ -3,7 +3,7 @@
     <template v-slot:main>
       <div>
         <div class="container-fluid">
-         
+
           <div class="mt-4">
             <div class="product-page row">
               <!-- Galerie d'images -->
@@ -21,20 +21,20 @@
                 <h3 class="text-black p-name">{{ marketDetails.name }}</h3>
                 <div class="stock-status">
                   <span class="text-success font-weight-bold" v-if="marketDetails.in_stock">{{ $t("general.in_stock")
-                    }}</span>
+                  }}</span>
                   <span class="text-danger" v-else>{{
                     $t("general.out_of_stock")
-                    }}</span>
+                  }}</span>
                 </div>
                 <div>
                   <label for="" class="text-black">{{ $t("general.availability") }}:</label>
                   <span class="ml-2">{{ $t("general.only") }} {{ marketDetails.quantity }}
                     <span class="" v-if="marketDetails.in_stock">{{
                       $t("general.in_stock")
-                      }}</span>
+                    }}</span>
                     <span class="text-danger" v-else>{{
                       $t("general.out_of_stock")
-                      }}</span></span>
+                    }}</span></span>
                 </div>
 
                 <div class="">
@@ -56,7 +56,7 @@
                   <div>
                     <span class="price">{{
                       marketDetails.price | locationPrice(rate, currencySelected)
-                      }}</span>
+                    }}</span>
                   </div>
                 </div>
 
@@ -76,28 +76,33 @@
                 </div>
               </div>
             </div>
-
             <div class="container" style="margin-top: 4em;">
-              <ul class="nav nav-pills mb-3 justify-content-around" id="pills-tab" role="tablist">
-                <li class="nav-item" role="presentation">
-                  <button class="nav-link active text-capitalize" id="pills-home-tab" data-toggle="pill"
-                    data-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">
-                    {{ $t("general.description") }}
-                  </button>
-                </li>
-                <li class="nav-item" role="presentation">
-                  <button class="nav-link" id="pills-profile-tab" data-toggle="pill" data-target="#pills-profile"
-                    type="button" role="tab" aria-controls="pills-profile" aria-selected="false">
-                    Specification
-                  </button>
-                </li>
-                <li class="nav-item" role="presentation">
-                  <button class="nav-link" id="pills-contact-tab" data-toggle="pill" data-target="#pills-contact"
-                    type="button" role="tab" aria-controls="pills-contact" aria-selected="false">
-                    Reviews
-                  </button>
-                </li>
-              </ul>
+              <ul class="nav nav-pills mb-3 justify-content-between flex-nowrap overflow-auto" id="pills-tab" role="tablist">
+  <li class="nav-item mx-1" role="presentation">
+    <button class="nav-link active text-capitalize px-2 px-md-3" id="pills-home-tab" data-toggle="pill"
+      data-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">
+      {{ $t("general.description") }}
+    </button>
+  </li>
+  <li class="nav-item mx-1" role="presentation">
+    <button class="nav-link px-2 px-md-3" id="pills-profile-tab" data-toggle="pill" data-target="#pills-profile"
+      type="button" role="tab" aria-controls="pills-profile" aria-selected="false">
+      Specification
+    </button>
+  </li>
+  <li class="nav-item mx-1" role="presentation">
+    <button class="nav-link px-2 px-md-3" id="pills-contact-tab" data-toggle="pill" data-target="#pills-contact"
+      type="button" role="tab" aria-controls="pills-contact" aria-selected="false">
+      Reviews
+    </button>
+  </li>
+  <li class="nav-item mx-1" role="presentation">
+    <button class="nav-link px-2 px-md-3" id="pills-delivery-tab" data-toggle="pill" data-target="#pills-delivery"
+      type="button" role="tab" aria-controls="pills-delivery" aria-selected="false">
+      Delivery Delay
+    </button>
+  </li>
+</ul>
               <div class="tab-content" id="pills-tabContent">
                 <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
                   <p>
@@ -106,11 +111,24 @@
                 </div>
                 <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab"></div>
                 <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab"></div>
+                <div class="tab-pane fade" id="pills-delivery" role="tabpanel" aria-labelledby="pills-delivery-tab">
+                  <div class="delivery-delays mt-3">
+                    <ul>
+                      <li>France (DOM) from 15 to 21 days.</li>
+                      <li>France from 15 to 21 days.</li>
+                      <li>United States from 15 to 21 days.</li>
+                      <li>Africa from 20 to 26 days.</li>
+                      <li>Europe from 15 to 21 days.</li>
+                      <li>Australia (Oceania) from 15 to 21 days.</li>
+                      <li>International from 15 to 21 days.</li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-       
+
       </div>
     </template>
   </base-layout>
@@ -240,7 +258,29 @@ export default {
 .btn {
   height: 50px;
 }
-
+.delivery-delays{
+  color: #455a64;
+}
+@media (max-width: 575.98px) {
+  .nav-pills .nav-link {
+    font-size: 13px;
+    white-space: nowrap;
+  }
+  
+  .nav-pills {
+    padding-bottom: 5px;
+  }
+  
+  /* Add smooth scrolling for horizontal nav */
+  .overflow-auto {
+    -webkit-overflow-scrolling: touch;
+    scrollbar-width: none; /* Firefox */
+  }
+  
+  .overflow-auto::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Edge */
+  }
+}
 @media screen and (max-width: 768px) {
   .p-name {
     font-size: 1.2em;

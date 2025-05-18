a8fe509c59cd76911fb612eb13b3ffae4891ec97
sanaiqbal5 <sana.iqbal.siqbal@gmail.com>
2025-05-19 01:22:39 +0500

    resolved country component issue on start

--- src/store/localisation.js
+++ src/store/localisation.js
@@ -80,55 +80,93 @@ export default {
       commit("setSelectedCountryCapital", capital)
 
     },
-
+    
     async startLocalisation({ commit }) {
-
-      const countries = currencyMap
-      const userCountry = await checkCountryLocalisation()
-      // const findCountryInfo = countries.find(u => u.sigle == userCountry)
-      const findCountryInfo = countries[userCountry];
-      const rate = await getRate((Object.keys(findCountryInfo.currency))[0], 'XAF')
-      console.log("+++++++++++",rate)
-
-      const constrySelected = localStorage.getItem("countrySelected")
-      const currencySelected = localStorage.getItem("currencySelected")
-      const selectedCountryCapital = localStorage.getItem("selectedCountryCapital")
-
-      if (constrySelected) {
-        commit("setSelectedCountry", JSON.parse(constrySelected))
-      } else {
-        commit("setSelectedCountry", findCountryInfo)
+      // Set loading state while we initialize
+      commit("setLoading", true);
+    
+      try {
+        const countries = currencyMap;
+        const userCountry = await checkCountryLocalisation();
+        // Get the country info from the map
+        const findCountryInfo = countries[userCountry];
+        
+        // Get exchange rate
+        const rate = await getRate((Object.keys(findCountryInfo.currency))[0], 'XAF');
+        
+        // Get stored values from localStorage if available
+        const constrySelected = localStorage.getItem("countrySelected");
+        const currencySelected = localStorage.getItem("currencySelected");
+        const selectedCountryCapital = localStorage.getItem("selectedCountryCapital");
+    
+        // Create properly formatted country object with all required properties
+        const formattedCountry = {
+          ...findCountryInfo,
+          code: findCountryInfo.code || findCountryInfo.sigle || userCountry,
+          flag: findCountryInfo.flag || `https://flagcdn.com/w80/${userCountry.toLowerCase()}.png`,
+          name: findCountryInfo.name || userCountry
+        };
+    
+        // Use stored country or fallback to our formatted country
+        if (constrySelected) {
+          const parsedCountry = JSON.parse(constrySelected);
+          // Ensure flag is available
+          if (!parsedCountry.flag) {
+            parsedCountry.flag = `https://flagcdn.com/w80/${(parsedCountry.code || parsedCountry.sigle || userCountry).toLowerCase()}.png`;
+          }
+          commit("setSelectedCountry", parsedCountry);
+        } else {
+          commit("setSelectedCountry", formattedCountry);
+        }
+    
+        // Handle currency selection
+        if (currencySelected) {
+          commit("setSelectedCurrency", JSON.parse(currencySelected));
+        } else {
+          const currencyKey = Object.keys(findCountryInfo.currency)[0];
+          commit("setSelectedCurrency", {
+            ...findCountryInfo.currency[currencyKey],
+            name: currencyKey
+          });
+        }
+    
+        // Handle capital selection
+        if (selectedCountryCapital) {
+          commit("setSelectedCountryCapital", JSON.parse(selectedCountryCapital));
+        } else {
+          // If findCountryInfo.capital is a string, use it directly
+          const capital = typeof findCountryInfo.capital === 'string' 
+            ? findCountryInfo.capital 
+            : (findCountryInfo.capital && findCountryInfo.capital.length > 0 
+              ? findCountryInfo.capital[0] 
+              : '');
+              
+          commit("setSelectedCountryCapital", capital);
+        }
+    
+        // Set other state values
+        commit("setRate", rate);
+        commit("setContryLocatisation", formattedCountry);
+        
+        // Sort countries and set them in the store
+        const sortedCountries = Object.values(countries)
+          .map(country => ({
+            ...country,
+            // Ensure code and flag exist for all countries
+            code: country.code || country.sigle,
+            flag: country.flag || `https://flagcdn.com/w80/${(country.code || country.sigle).toLowerCase()}.png`
+          }))
+          .sort((a, b) => a.name.localeCompare(b.name));
+          
+        commit("setContries", sortedCountries);
+        
+        // Mark loading as complete
+        commit("setHasBeenLoad");
+      } catch (error) {
+        console.error("Error initializing localization:", error);
+      } finally {
+        commit("setLoading", false);
       }
-
-      if (currencySelected) {
-        commit("setSelectedCurrency", JSON.parse(currencySelected))
-
-      } else {
-        commit("setSelectedCurrency", {
-          ...findCountryInfo.currency[(Object.keys(findCountryInfo.currency))[0]],
-          name: Object.keys(findCountryInfo.currency)[0]
-        })
-      }
-      if (selectedCountryCapital) {
-        commit("setSelectedCountryCapital", JSON.parse(selectedCountryCapital));
-      } else {
-        // If findCountryInfo.capital is a string, use it directly
-        const capital = typeof findCountryInfo.capital === 'string' 
-          ? findCountryInfo.capital 
-          : (findCountryInfo.capital && findCountryInfo.capital.length > 0 
-            ? findCountryInfo.capital[0] 
-            : '');
-            
-        commit("setSelectedCountryCapital", capital);
-      }
-
-      commit("setRate", rate)
-      commit("setContryLocatisation", findCountryInfo)
-      commit("setContries", countries.sort((a, b) => a.name.localeCompare(b.name)))
-
-      commit("setHasBeenLoad")
-
-      commit("setLoading", false)
     },
     settingsUpdated({ commit }, settings) {
       // Update the timestamp to trigger reactivity

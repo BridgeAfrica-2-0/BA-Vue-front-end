ca3930eba0366f3009fa4d25367e2c722d703310
sanaiqbal5 <sana.iqbal.siqbal@gmail.com>
2025-06-03 18:03:00 +0500

    camera access updated

--- src/components/businessOwner/ProductImageUpload.vue
+++ src/components/businessOwner/ProductImageUpload.vue
@@ -124,9 +124,11 @@
     </div>
     <!-- <input type="file" id="hidden-image-input" multiple accept="image/*" style="display: none;" @change="onFileSelect">
     <input type="file" id="hidden-video-input" accept="video/*" style="display: none;" @change="handleVideoUpload"> -->
-    <input type="file" id="hidden-image-input" multiple accept=".jpg,.jpeg,.png,.gif" 
+    <input type="file" id="hidden-image-input" multiple accept=".jpg,.jpeg,.png,.gif"  data-capture="false"
+       webkitdirectory="false"
       style="display: none;" @change="onFileSelect">
-    <input type="file" id="hidden-video-input" accept=".mp4,.avi,.mov,.wmv,.flv,.webm,.mkv,.m4v"  style="display: none;"
+    <input type="file" id="hidden-video-input" accept=".mp4,.avi,.mov,.wmv,.flv,.webm,.mkv,.m4v"  data-capture="false"
+    webkitdirectory="false" style="display: none;"
       @change="handleVideoUpload">
       <!-- <input type="file" id="hidden-image-input" multiple 
        accept="image/jpeg,image/jpg,image/png,image/gif,image/bmp,image/webp"
